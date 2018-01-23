1b95a91e3e722b25b02b082eacc0a325d3b3896d
Kwang Yul Seo <kseo@kodebox.io>
2018-01-23 13:50:27 +0900

    Setup logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    debug!("this is a debug {}", "message");
+    error!("this is printed by default");
 }
