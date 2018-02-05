3a921dc972d9e116a0f96c5a96a06a9b786a419e
Kwang Yul Seo <kseo@kodebox.io>
2018-02-05 15:16:42 +0900

    Add codechain-types package

---
+++ primitives/codechain-types/src/lib.rs
@@ -0,0 +1,28 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(asm_available, feature(asm))]
+
+#[cfg(feature = "std")]
+extern crate core;
+#[macro_use]
+extern crate crunchy;
+#[macro_use]
+extern crate fixed_hash;
+#[macro_use]
+extern crate uint as uint_crate;
+
+#[cfg(feature = "serialize")]
+extern crate codechain_types_serialize;
+#[cfg(feature = "serialize")]
+extern crate serde;
+
+mod hash;
+mod uint;
+
+pub use uint::{U128, U256, U512};
+pub use hash::{H1024, H128, H160, H256, H264, H32, H512, H520, H64};
+pub use fixed_hash::clean_0x;
+
+pub type Address = H160;
+pub type Secret = H256;
+pub type Public = H512;
+pub type Signature = H520;
