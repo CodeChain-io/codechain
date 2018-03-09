d0eb8a4b92a0e945030aaf879ca67047c8746d8b
Kwang Yul Seo <kseo@kodebox.io>
2018-03-10 00:42:30 +0900

    Remove IntoBase32 trait
    The trait name is misleading because it does not perform conversion to
    base32 encoding. It simply rearranges bits into group of 5.

--- keys/src/address.rs
+++ keys/src/address.rs
@@ -29,25 +29,31 @@ pub struct Address {
     pub account_id: AccountId,
 }
 
-trait IntoBase32 {
-    fn into_base32(&self) -> Vec<u8>;
+fn rearrange_bits_into_group_of_5(data: &[u8; 20]) -> Vec<u8> {
+    let mut vec = Vec::new();
+    for x in 0..4 {
+        vec.push(((data[x * 5 + 0] & 0b11111000) >> 3));
+        vec.push(((data[x * 5 + 0] & 0b00000111) << 2) | ((data[x * 5 + 1] & 0b11000000) >> 6));
+        vec.push(((data[x * 5 + 1] & 0b00111110) >> 1));
+        vec.push(((data[x * 5 + 1] & 0b00000001) << 4) | ((data[x * 5 + 2] & 0b11110000) >> 4));
+        vec.push(((data[x * 5 + 2] & 0b00001111) << 1) | ((data[x * 5 + 3] & 0b10000000) >> 7));
+        vec.push(((data[x * 5 + 3] & 0b01111100) >> 2));
+        vec.push(((data[x * 5 + 3] & 0b00000011) << 3) | ((data[x * 5 + 4] & 0b11100000) >> 5));
+        vec.push(((data[x * 5 + 4] & 0b00011111) >> 0));
+    }
+    vec
 }
 
-impl IntoBase32 for AccountId {
-    fn into_base32(&self) -> Vec<u8> {
-        let mut vec = Vec::new();
-        for x in 0..4 {
-            vec.push(((self[x * 5 + 0] & 0b11111000) >> 3));
-            vec.push(((self[x * 5 + 0] & 0b00000111) << 2) | ((self[x * 5 + 1] & 0b11000000) >> 6));
-            vec.push(((self[x * 5 + 1] & 0b00111110) >> 1));
-            vec.push(((self[x * 5 + 1] & 0b00000001) << 4) | ((self[x * 5 + 2] & 0b11110000) >> 4));
-            vec.push(((self[x * 5 + 2] & 0b00001111) << 1) | ((self[x * 5 + 3] & 0b10000000) >> 7));
-            vec.push(((self[x * 5 + 3] & 0b01111100) >> 2));
-            vec.push(((self[x * 5 + 3] & 0b00000011) << 3) | ((self[x * 5 + 4] & 0b11100000) >> 5));
-            vec.push(((self[x * 5 + 4] & 0b00011111) >> 0));
-        }
-        vec
+fn rearrange_bits_into_group_of_8(data: &Vec<u8>) -> [u8; 20] {
+    let mut arr = [0u8; 20];
+    for x in 0..4 {
+        arr[x * 5 + 0] = ((data[x * 8 + 0] & 0b00011111) << 3) | ((data[x * 8 + 1] & 0b00011100) >> 2);
+        arr[x * 5 + 1] = ((data[x * 8 + 1] & 0b00000011) << 6) | ((data[x * 8 + 2] & 0b00011111) << 1) | ((data[x * 8 + 3] & 0b00010000) >> 4);
+        arr[x * 5 + 2] = ((data[x * 8 + 3] & 0b00001111) << 4) | ((data[x * 8 + 4] & 0b00011110) >> 1);
+        arr[x * 5 + 3] = ((data[x * 8 + 4] & 0b00000001) << 7) | ((data[x * 8 + 5] & 0b00011111) << 2) | ((data[x * 8 + 6] & 0b00011000) >> 3);
+        arr[x * 5 + 4] = ((data[x * 8 + 6] & 0b00000111) << 5) | ((data[x * 8 + 7] & 0b00011111) >> 0);
     }
+    arr
 }
 
 impl fmt::Display for Address {
@@ -58,7 +64,7 @@ impl fmt::Display for Address {
         };
         let encode_result = Bech32 {
             hrp: hrp.to_string(),
-            data: self.account_id.into_base32(),
+            data: rearrange_bits_into_group_of_5(&self.account_id.into()),
         }.to_string();
         write!(f, "{}", encode_result.unwrap())
     }
@@ -76,17 +82,9 @@ impl FromStr for Address {
         };
         match network {
             Some(network) => {
-                let mut arr = [0u8; 20];
-                for x in 0..4 {
-                    arr[x * 5 + 0] = ((decoded.data[x * 8 + 0] & 0b00011111) << 3) | ((decoded.data[x * 8 + 1] & 0b00011100) >> 2);
-                    arr[x * 5 + 1] = ((decoded.data[x * 8 + 1] & 0b00000011) << 6) | ((decoded.data[x * 8 + 2] & 0b00011111) << 1) | ((decoded.data[x * 8 + 3] & 0b00010000) >> 4);
-                    arr[x * 5 + 2] = ((decoded.data[x * 8 + 3] & 0b00001111) << 4) | ((decoded.data[x * 8 + 4] & 0b00011110) >> 1);
-                    arr[x * 5 + 3] = ((decoded.data[x * 8 + 4] & 0b00000001) << 7) | ((decoded.data[x * 8 + 5] & 0b00011111) << 2) | ((decoded.data[x * 8 + 6] & 0b00011000) >> 3);
-                    arr[x * 5 + 4] = ((decoded.data[x * 8 + 6] & 0b00000111) << 5) | ((decoded.data[x * 8 + 7] & 0b00011111) >> 0);
-                }
                 Ok(Address {
-                    network: network,
-                    account_id: H160(arr),
+                    network,
+                    account_id: H160(rearrange_bits_into_group_of_8(&decoded.data)),
                 })
             }
             None => Err(Error::Bech32UnknownHRP)
