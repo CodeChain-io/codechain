476fc52f9a9195db789b2b29365cb8c37613e7ca
JinGyeong Jeong <jjg@kodebox.io>
2018-03-12 13:41:43 +0900

    Refine rearrange_bits_into_group_of_(5|8)

--- keys/src/address.rs
+++ keys/src/address.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use std::cmp;
 use std::fmt;
 use std::str::FromStr;
 use network::Network;
@@ -38,31 +39,38 @@ impl Address {
     }
 }
 
-fn rearrange_bits_into_group_of_5(data: &[u8; 20]) -> Vec<u8> {
-    let mut vec = Vec::new();
-    for x in 0..4 {
-        vec.push(((data[x * 5 + 0] & 0b11111000) >> 3));
-        vec.push(((data[x * 5 + 0] & 0b00000111) << 2) | ((data[x * 5 + 1] & 0b11000000) >> 6));
-        vec.push(((data[x * 5 + 1] & 0b00111110) >> 1));
-        vec.push(((data[x * 5 + 1] & 0b00000001) << 4) | ((data[x * 5 + 2] & 0b11110000) >> 4));
-        vec.push(((data[x * 5 + 2] & 0b00001111) << 1) | ((data[x * 5 + 3] & 0b10000000) >> 7));
-        vec.push(((data[x * 5 + 3] & 0b01111100) >> 2));
-        vec.push(((data[x * 5 + 3] & 0b00000011) << 3) | ((data[x * 5 + 4] & 0b11100000) >> 5));
-        vec.push(((data[x * 5 + 4] & 0b00011111) >> 0));
+fn rearrange_bits(data: &Vec<u8>, from: usize, into: usize) -> Vec<u8> {
+    let mut vec = Vec::with_capacity((data.len() * from + (into - 1)) / into);
+
+    let mut group_index = 0;
+    let mut group_required_bits = into;
+
+    for val in data.iter() {
+        let mut ungrouped_bits= from;
+
+        while ungrouped_bits > 0 {
+            let min = cmp::min(group_required_bits, ungrouped_bits);
+            let min_mask = (1 << min) - 1;
+
+            if group_required_bits == into {
+                vec.push(0);
+            }
+
+            if ungrouped_bits >= group_required_bits {
+                vec[group_index] |= (val >> (ungrouped_bits - group_required_bits)) & min_mask;
+            } else {
+                vec[group_index] |= (val & min_mask) << (group_required_bits - ungrouped_bits);
+            }
+
+            group_required_bits -= min;
+            if group_required_bits == 0 {
+                group_index += 1;
+                group_required_bits = into;
+            }
+            ungrouped_bits -= min;
+        }
     }
     vec
-}
-
-fn rearrange_bits_into_group_of_8(data: &Vec<u8>) -> [u8; 20] {
-    let mut arr = [0u8; 20];
-    for x in 0..4 {
-        arr[x * 5 + 0] = ((data[x * 8 + 0] & 0b00011111) << 3) | ((data[x * 8 + 1] & 0b00011100) >> 2);
-        arr[x * 5 + 1] = ((data[x * 8 + 1] & 0b00000011) << 6) | ((data[x * 8 + 2] & 0b00011111) << 1) | ((data[x * 8 + 3] & 0b00010000) >> 4);
-        arr[x * 5 + 2] = ((data[x * 8 + 3] & 0b00001111) << 4) | ((data[x * 8 + 4] & 0b00011110) >> 1);
-        arr[x * 5 + 3] = ((data[x * 8 + 4] & 0b00000001) << 7) | ((data[x * 8 + 5] & 0b00011111) << 2) | ((data[x * 8 + 6] & 0b00011000) >> 3);
-        arr[x * 5 + 4] = ((data[x * 8 + 6] & 0b00000111) << 5) | ((data[x * 8 + 7] & 0b00011111) >> 0);
-    }
-    arr
 }
 
 impl fmt::Display for Address {
@@ -73,7 +81,7 @@ impl fmt::Display for Address {
         };
         let encode_result = Bech32 {
             hrp: hrp.to_string(),
-            data: rearrange_bits_into_group_of_5(&self.account_id.into()),
+            data: rearrange_bits(&self.account_id.to_vec(), 8, 5),
         }.to_string();
         write!(f, "{}", encode_result.unwrap())
     }
@@ -93,7 +101,14 @@ impl FromStr for Address {
             Some(network) => {
                 Ok(Address {
                     network,
-                    account_id: H160(rearrange_bits_into_group_of_8(&decoded.data)),
+                    account_id: {
+                        let vec = rearrange_bits(&decoded.data, 5, 8);
+                        let mut arr = [0u8; 20];
+                        for i in 0..20 {
+                            arr[i] = vec[i];
+                        }
+                        H160(arr)
+                    },
                 })
             }
             None => Err(Error::Bech32UnknownHRP)
