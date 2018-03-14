8c150d9749011e6313bc13724f2e016ce6e3cf11
Seulgi Kim <seulgi.kim@kodebox.io>
2018-03-14 11:09:07 +0900

    Use alias when importing codechain packages

--- network/src/kademlia/node_id.rs
+++ network/src/kademlia/node_id.rs
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use codechain_types::Public;
+use ctypes::Public;
 use super::B;
 
 pub type NodeId = Public;
