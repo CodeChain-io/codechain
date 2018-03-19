77df3cb75e5c9fec7fdc04f79af286a1638d59df
Kwang Yul Seo <kseo@kodebox.io>
2018-03-19 17:54:17 +0900

    Implement db::Key for BlockNumber and H256

--- core/src/blockchain/extras.rs
+++ core/src/blockchain/extras.rs
@@ -14,11 +14,72 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use ctypes::{H256, U256};
+use std::ops::Deref;
+
+use ctypes::{H256, H264, U256};
 
 use heapsize::HeapSizeOf;
 
+use super::super::db::Key;
 use super::super::header::BlockNumber;
+
+/// Represents index of extra data in database
+#[derive(Copy, Debug, Hash, Eq, PartialEq, Clone)]
+pub enum ExtrasIndex {
+    /// Block details index
+    BlockDetails = 0,
+    /// Block hash index
+    BlockHash = 1,
+    /// Transaction address index
+    TransactionAddress = 2,
+}
+
+fn with_index(hash: &H256, i: ExtrasIndex) -> H264 {
+    let mut result = H264::default();
+    result[0] = i as u8;
+    (*result)[1..].clone_from_slice(hash);
+    result
+}
+
+pub struct BlockNumberKey([u8; 5]);
+
+impl Deref for BlockNumberKey {
+    type Target = [u8];
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Key<H256> for BlockNumber {
+    type Target = BlockNumberKey;
+
+    fn key(&self) -> Self::Target {
+        let mut result = [0u8; 5];
+        result[0] = ExtrasIndex::BlockHash as u8;
+        result[1] = (self >> 24) as u8;
+        result[2] = (self >> 16) as u8;
+        result[3] = (self >> 8) as u8;
+        result[4] = *self as u8;
+        BlockNumberKey(result)
+    }
+}
+
+impl Key<BlockDetails> for H256 {
+    type Target = H264;
+
+    fn key(&self) -> H264 {
+        with_index(self, ExtrasIndex::BlockDetails)
+    }
+}
+
+impl Key<TransactionAddress> for H256 {
+    type Target = H264;
+
+    fn key(&self) -> H264 {
+        with_index(self, ExtrasIndex::TransactionAddress)
+    }
+}
 
 /// Familial details concerning a block
 #[derive(Debug, Clone, RlpEncodable, RlpDecodable)]
