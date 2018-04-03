d0cd9ed759bac3976db9d1b7a3e4ade12acf20fd
Kwang Yul Seo <kseo@kodebox.io>
2018-04-03 16:55:38 +0900

    Add create_test_block_with_data test helper function

--- core/src/tests/helpers.rs
+++ core/src/tests/helpers.rs
@@ -24,6 +24,7 @@ use rlp::{self, RlpStream};
 use spec::Spec;
 use state::State;
 use state_db::StateDB;
+use transaction::SignedTransaction;
 
 pub fn get_test_spec() -> Spec {
     Spec::new_solo()
@@ -33,6 +34,17 @@ pub fn create_test_block(header: &Header) -> Bytes {
     let mut rlp = RlpStream::new_list(2);
     rlp.append(header);
     rlp.append_raw(&rlp::EMPTY_LIST_RLP, 1);
+    rlp.out()
+}
+
+pub fn create_test_block_with_data(header: &Header, transactions: &[SignedTransaction], uncles: &[Header]) -> Bytes {
+    let mut rlp = RlpStream::new_list(3);
+    rlp.append(header);
+    rlp.begin_list(transactions.len());
+    for t in transactions {
+        rlp.append_raw(&rlp::encode(t).into_vec(), 1);
+    }
+    rlp.append_list(&uncles);
     rlp.out()
 }
 
