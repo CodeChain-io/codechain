7fb6690184865848d9132111bd1cab66eb47c785
Seulgi Kim <seulgi.kim@kodebox.io>
2018-04-10 11:25:22 +0900

    Allow deprecated for network package
    Until converting mio::EventLoop to mio::Poll.

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#![allow(deprecated)]
+
 #[macro_use]
 extern crate log;
 extern crate mio;
