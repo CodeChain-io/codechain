eb5611d28a61734514f9f4e3bb4ce04434c00f53
Seulgi Kim <seulgi.kim@kodebox.io>
2018-04-13 11:31:55 +0900

    Adjust the visibility of TestNetworkClient

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -39,11 +39,9 @@ mod extension;
 mod handshake;
 mod limited_table;
 mod service;
+mod test;
 mod timer_info;
 mod token_generator;
-
-#[cfg(test)]
-mod test;
 
 pub mod connection;
 pub mod session;
@@ -53,3 +51,4 @@ pub use self::config::Config as NetworkConfig;
 pub use self::discovery::Api as DiscoveryApi;
 pub use self::extension::{Api, Error, Extension as NetworkExtension, NodeToken, Result, TimerToken};
 pub use self::service::Service as NetworkService;
+pub use self::test::TestClient as TestNetworkClient;
