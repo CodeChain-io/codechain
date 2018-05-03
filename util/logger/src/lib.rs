ca91545175e4e43b1708ac1fa2b5ed080a8ee9ea
Joon-Mo Yang <jmyang@kodebox.io>
2018-05-03 20:24:11 +0900

    Introduce new logger for codechain

---
+++ util/logger/src/lib.rs
@@ -0,0 +1,31 @@
+// Copyright 2018 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+extern crate env_logger;
+extern crate log;
+
+mod logger;
+
+use log::SetLoggerError;
+
+pub use logger::Config as LoggerConfig;
+use logger::Logger;
+
+pub fn init(config: &LoggerConfig) -> Result<(), SetLoggerError> {
+    let logger = Logger::new(config);
+    log::set_max_level(logger.filter());
+    log::set_boxed_logger(Box::new(logger))
+}
---
+++ util/logger/src/logger.rs
@@ -0,0 +1,70 @@
+// Copyright 2018 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use env_logger::filter::{Builder as FilterBuilder, Filter};
+use log::{LevelFilter, Log, Metadata, Record};
+
+pub struct Config {
+    pub instance_id: usize,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            instance_id: SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .expect("Current time should be later than unix epoch")
+                .subsec_nanos() as usize,
+        }
+    }
+}
+
+pub struct Logger {
+    instance_id: usize,
+    filter: Filter,
+}
+
+impl Logger {
+    pub fn new(config: &Config) -> Self {
+        let mut builder = FilterBuilder::from_env("RUST_LOG");
+        builder.filter(None, LevelFilter::Info);
+
+        Self {
+            instance_id: config.instance_id,
+            filter: builder.build(),
+        }
+    }
+
+    pub fn filter(&self) -> LevelFilter {
+        self.filter.filter()
+    }
+}
+
+impl Log for Logger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        self.filter.enabled(metadata)
+    }
+
+    fn log(&self, record: &Record) {
+        if self.filter.matches(record) {
+            println!("{}", record.args());
+        }
+    }
+
+    fn flush(&self) {}
+}
