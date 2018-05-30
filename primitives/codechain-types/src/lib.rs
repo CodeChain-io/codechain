9fd39320aac3c964d04c02d09ff8fc219837e8d4
Seulgi Kim <seulgi.kim@kodebox.io>
2018-05-30 16:59:34 +0900

    Use ethereum-types for big integer

--- primitives/codechain-types/src/lib.rs
+++ primitives/codechain-types/src/lib.rs
@@ -1,26 +1,7 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-#![cfg_attr(asm_available, feature(asm))]
+extern crate ethereum_types;
 
-#[cfg(feature = "std")]
-extern crate core;
-#[macro_use]
-extern crate crunchy;
-#[macro_use]
-extern crate fixed_hash;
-#[macro_use]
-extern crate uint as uint_crate;
-
-#[cfg(feature = "serialize")]
-extern crate codechain_types_serialize;
-#[cfg(feature = "serialize")]
-extern crate serde;
-
-pub mod hash;
-mod uint;
-
-pub use fixed_hash::clean_0x;
-pub use hash::{H1024, H128, H160, H256, H264, H32, H512, H520, H64};
-pub use uint::{U128, U256, U512};
+pub use ethereum_types::{H1024, H128, H160, H256, H264, H32, H512, H520, H64};
+pub use ethereum_types::{U128, U256, U512};
 
 pub type Address = H160;
 pub type Secret = H256;
