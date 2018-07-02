55840ce19bd47a0143b189b382701fe7c727be19
Seulgi Kim <seulgi.kim@kodebox.io>
2018-07-02 19:08:29 +0900

    Remove overwrite_with method of cache entry
    The method just moves the value.

--- core/src/state/cache.rs
+++ core/src/state/cache.rs
@@ -93,19 +93,6 @@ where
             state: EntryState::CleanCached,
         }
     }
-
-    // Replace data with another entry but preserve storage cache.
-    fn overwrite_with(&mut self, other: Self) {
-        self.state = other.state;
-        match other.item {
-            Some(acc) => {
-                if let Some(ref mut ours) = self.item {
-                    *ours = acc;
-                }
-            }
-            None => self.item = None,
-        }
-    }
 }
 
 
@@ -152,18 +139,14 @@ where
         if let Some(mut checkpoint) = self.checkpoints.get_mut().pop() {
             for (k, v) in checkpoint.drain() {
                 match v {
-                    Some(v) => {
-                        match self.cache.get_mut().entry(k) {
-                            HashMapEntry::Occupied(mut e) => {
-                                // Merge checkpointed changes back into the main account
-                                // storage preserving the cache.
-                                e.get_mut().overwrite_with(v);
-                            }
-                            HashMapEntry::Vacant(e) => {
-                                e.insert(v);
-                            }
+                    Some(v) => match self.cache.get_mut().entry(k) {
+                        HashMapEntry::Occupied(mut e) => {
+                            *e.get_mut() = v;
                         }
-                    }
+                        HashMapEntry::Vacant(e) => {
+                            e.insert(v);
+                        }
+                    },
                     None => {
                         if let HashMapEntry::Occupied(e) = self.cache.get_mut().entry(k) {
                             if e.get().is_dirty() {
