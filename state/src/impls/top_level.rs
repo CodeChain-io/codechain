3fd127ef3b0f384c57f883f5bcf50fd390b61e8a
Seulgi Kim <dev@seulgi.kim>
2018-07-20 16:50:59 +0900

    Extract a function to execute transactions
    Currently, apply_transactions function check previous root, execute
    transactions, and check post root.
    
    This patch extracts the method to execute transactions.

--- state/src/impls/top_level.rs
+++ state/src/impls/top_level.rs
@@ -348,9 +348,9 @@ impl<B: Backend + TopBackend + ShardBackend + Clone> TopLevelState<B> {
                     }
                 }
 
-                let first_result = self.apply_transactions(&transactions, &changes[0])?;
+                let first_result = self.apply_transactions_with_check(&transactions, &changes[0])?;
                 for change in changes.iter().skip(1) {
-                    let result = self.apply_transactions(&transactions, change)?;
+                    let result = self.apply_transactions_with_check(&transactions, change)?;
                     if result != first_result {
                         return Err(ParcelError::InconsistentShardOutcomes.into())
                     }
@@ -395,7 +395,7 @@ impl<B: Backend + TopBackend + ShardBackend + Clone> TopLevelState<B> {
         }
     }
 
-    fn apply_transactions(
+    fn apply_transactions_with_check(
         &mut self,
         transactions: &[Transaction],
         change: &ChangeShard,
@@ -410,16 +410,7 @@ impl<B: Backend + TopBackend + ShardBackend + Clone> TopLevelState<B> {
             }).into())
         }
 
-        // FIXME: Make it mutable borrow db instead of cloning.
-        let mut shard_level_state =
-            ShardLevelState::from_existing(shard_id, self.db.clone(), shard_root, self.trie_factory)?;
-
-        let mut results = Vec::with_capacity(transactions.len());
-        for t in transactions {
-            let result = shard_level_state.apply(t)?;
-            results.push(result);
-        }
-        let (new_shard_root, db) = shard_level_state.drop();
+        let (new_shard_root, db, results) = self.apply_transactions(transactions, shard_id, shard_root)?;
 
         if !change.post_root.is_zero() && change.post_root != new_shard_root {
             return Err(ParcelError::InvalidShardRoot(Mismatch {
@@ -432,6 +423,26 @@ impl<B: Backend + TopBackend + ShardBackend + Clone> TopLevelState<B> {
 
         self.set_shard_root(shard_id, &shard_root, &new_shard_root)?;
         Ok(results)
+    }
+
+    pub fn apply_transactions(
+        &mut self,
+        transactions: &[Transaction],
+        shard_id: u32,
+        shard_root: H256,
+    ) -> StateResult<(H256, B, Vec<TransactionOutcome>)> {
+        // FIXME: Make it mutable borrow db instead of cloning.
+        let mut shard_level_state =
+            ShardLevelState::from_existing(shard_id, self.db.clone(), shard_root, self.trie_factory)?;
+
+        let mut results = Vec::with_capacity(transactions.len());
+        for t in transactions {
+            let result = shard_level_state.apply(t)?;
+            results.push(result);
+        }
+
+        let (new_root, db) = shard_level_state.drop();
+        Ok((new_root, db, results))
     }
 
     fn create_shard_level_state(&mut self) -> StateResult<()> {
