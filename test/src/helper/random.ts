a6852c423db1e830c376400ed3ae91be539a7f20
Hyunsik Jeong <rbtree@kodebox.io>
2018-09-05 16:41:10 +0900

    Create log files from tests

--- test/src/helper/random.ts
+++ test/src/helper/random.ts
@@ -30,6 +30,14 @@ export function makeRandomH256() {
   return text;
 }
 
+export function makeRandomFilename(suffix: string) {
+  let text = "";
+  const possible = "abcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 10; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  return text + suffix;
+}
+
 export function getRandomIndex(size: number) {
   return Math.floor(Math.random() * size);
 }
