6d7c5d79a07121d4db39810df939ccd4308cca77
Hyunsik Jeong <rbtree@kodebox.io>
2018-09-11 14:23:23 +0900

    Add tests on sending invalid parcels

--- test/src/integration/basic.test.ts
+++ test/src/integration/basic.test.ts
@@ -14,9 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { SDK } from "codechain-sdk";
-import { PlatformAddress } from "codechain-sdk/lib/key/PlatformAddress";
-
 import CodeChain from "../helper/spawn";
 
 describe("solo - 1 node", () => {
