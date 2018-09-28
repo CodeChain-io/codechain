49409a69aecea1bfc77f18a1dadf16560b4d7089
Seulgi Kim <seulgi.kim@kodebox.io>
2018-09-28 10:48:41 +0900

    Add latest_regular_key

--- core/src/client/mod.rs
+++ core/src/client/mod.rs
@@ -160,6 +160,9 @@ pub trait Balance {
 
 pub trait RegularKey {
     fn regular_key(&self, address: &Address, state: StateOrBlock) -> Option<Public>;
+    fn latest_regular_key(&self, address: &Address) -> Option<Public> {
+        self.regular_key(address, BlockId::Latest.into())
+    }
 }
 
 pub trait RegularKeyOwner {
