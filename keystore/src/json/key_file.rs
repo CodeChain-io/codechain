371e0044afc695ce3928ed4e96fd89f83e3efd35
Seulgi Kim <seulgi.kim@kodebox.io>
2018-10-04 14:26:36 +0900

    Make the account field of keystore optional

--- keystore/src/json/key_file.rs
+++ keystore/src/json/key_file.rs
@@ -51,7 +51,7 @@ pub struct KeyFile {
     pub id: Uuid,
     pub version: Version,
     pub crypto: Crypto,
-    pub address: H160,
+    pub address: Option<H160>,
     pub name: Option<String>,
     pub meta: Option<String>,
 }
@@ -108,7 +108,7 @@ mod tests {
         let expected = KeyFile {
             id: Uuid::from_str("8777d9f6-7860-4b9b-88b7-0b57ee6b3a73").unwrap(),
             version: Version::V1,
-            address: "6edddfc6349aff20bc6467ccf276c5b52487f7a8".into(),
+            address: Some("6edddfc6349aff20bc6467ccf276c5b52487f7a8".into()),
             crypto: Crypto {
                 cipher: Cipher::Aes128Ctr(Aes128Ctr {
                     iv: "b5a7ec855ec9e2c405371356855fec83".into(),
@@ -128,6 +128,58 @@ mod tests {
         };
 
         let keyfile: KeyFile = serde_json::from_str(json).unwrap();
+        assert_eq!(keyfile, expected);
+    }
+
+    #[test]
+    fn address_is_an_optional_field() {
+        const JSON: &str = r#"
+        {
+            "crypto": {
+                "cipher": "aes-128-ctr",
+                "ciphertext": "7203da0676d141b138cd7f8e1a4365f59cc1aa6978dc5443f364ca943d7cb4bc",
+                "cipherparams": {
+                    "iv": "b5a7ec855ec9e2c405371356855fec83"
+                },
+                "kdf": "scrypt",
+                "kdfparams": {
+                    "dklen": 32,
+                    "n": 262144,
+                    "p": 1,
+                    "r": 8,
+                    "salt": "1e8642fdf1f87172492c1412fc62f8db75d796cdfa9c53c3f2b11e44a2a1b209"
+                },
+                "mac": "46325c5d4e8c991ad2683d525c7854da387138b6ca45068985aa4959fa2b8c8f"
+            },
+            "id": "8777d9f6-7860-4b9b-88b7-0b57ee6b3a73",
+            "version": 1,
+            "name": "Test",
+            "meta": "{}"
+        }"#;
+
+        let expected = KeyFile {
+            id: Uuid::from_str("8777d9f6-7860-4b9b-88b7-0b57ee6b3a73").unwrap(),
+            version: Version::V1,
+            address: None,
+            crypto: Crypto {
+                cipher: Cipher::Aes128Ctr(Aes128Ctr {
+                    iv: "b5a7ec855ec9e2c405371356855fec83".into(),
+                }),
+                ciphertext: "7203da0676d141b138cd7f8e1a4365f59cc1aa6978dc5443f364ca943d7cb4bc".into(),
+                kdf: Kdf::Scrypt(Scrypt {
+                    n: 262144,
+                    dklen: 32,
+                    p: 1,
+                    r: 8,
+                    salt: "1e8642fdf1f87172492c1412fc62f8db75d796cdfa9c53c3f2b11e44a2a1b209".into(),
+                }),
+                mac: "46325c5d4e8c991ad2683d525c7854da387138b6ca45068985aa4959fa2b8c8f".into(),
+            },
+            name: Some("Test".to_string()),
+            meta: Some("{}".to_string()),
+        };
+
+        let keyfile: KeyFile = serde_json::from_str(JSON).unwrap();
         assert_eq!(keyfile, expected);
     }
 
@@ -167,7 +219,7 @@ mod tests {
         let file = KeyFile {
             id: "8777d9f6-7860-4b9b-88b7-0b57ee6b3a73".into(),
             version: Version::V1,
-            address: "6edddfc6349aff20bc6467ccf276c5b52487f7a8".into(),
+            address: Some("6edddfc6349aff20bc6467ccf276c5b52487f7a8".into()),
             crypto: Crypto {
                 cipher: Cipher::Aes128Ctr(Aes128Ctr {
                     iv: "b5a7ec855ec9e2c405371356855fec83".into(),
