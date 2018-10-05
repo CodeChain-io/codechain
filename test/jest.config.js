755816163bc05820b6494ff4af28b61472266371
Geunwoo Kim <posgnu@kodebox.io>
2018-10-05 16:51:12 +0900

    Add OnChain invalid parcel propagation tests

--- test/jest.config.js
+++ test/jest.config.js
@@ -1,17 +1,8 @@
 module.exports = {
-  roots: [
-    "<rootDir>/src"
-  ],
+  roots: ["<rootDir>/src"],
   transform: {
     "^.+\\.tsx?$": "ts-jest"
   },
   testRegex: "/src/.*\\.test\\.tsx?$",
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-}
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
