23477b2d92974777dc1aa2cbaff2c751354a7661
Juhyung Park <jh@kodebox.io>
2018-11-21 16:07:44 +0900

    Add integration test of Tendermint consensus

--- test/src/helper/constants.ts
+++ test/src/helper/constants.ts
@@ -47,3 +47,51 @@ export const bobAddress = SDK.Core.classes.PlatformAddress.fromAccountId(
 export const invalidSecret =
     "0000000000000000000000000000000000000000000000000000000000000000";
 export const invalidAddress = "tccqyqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqhhn9p3";
+
+export const validator0Secret =
+    "b05b7c1e9747330e97676a95f55d3e469794dfa2aaa3c958d2d3eb334da9fb55";
+export const validator0Public = SDK.util.getPublicFromPrivate(validator0Secret);
+// 4f1541fc6bdec60bf0ac6380a8e3914a469fe6cd4fa817c890d5823cfdda83932f61dc083e1b6736dadeceb5afd3fcfbac915e5fa2c9c20acf1c30b080114d7f
+export const validator0AccountId = SDK.util.getAccountIdFromPrivate(
+    validator0Secret
+); // 6a8e5ec34cdb3cde78ebf4dfd8d84f00f437fddb
+export const validator0Address = SDK.Core.classes.PlatformAddress.fromAccountId(
+    validator0AccountId,
+    { networkId: "tc" }
+); // tccq94guhkrfndnehnca06dlkxcfuq0gdlamvw9ga4f
+
+export const validator1Secret =
+    "79d26d5788ca5f5ae87e8dd0f057124c2cfda11136aeb140f1d9ac3648d5b703";
+export const validator1Public = SDK.util.getPublicFromPrivate(validator1Secret);
+// 1ac8248deb29a58c4bdbfce031fb22c7ba3bcc9384bf6de058a1c8bef5a17422cf8ca26666a5505684db7364eabeed6fc678b02658ae7c1848a4ae6e50244cf2
+export const validator1AccountId = SDK.util.getAccountIdFromPrivate(
+    validator1Secret
+); // c25b8e91fccd3b8b137b5faa7f86f656252ba2ee
+export const validator1Address = SDK.Core.classes.PlatformAddress.fromAccountId(
+    validator1AccountId,
+    { networkId: "tc" }
+); // tccq8p9hr53lnxnhzcn0d065lux7etz22azaca786tt
+
+export const validator2Secret =
+    "83352d249f5fe8d85b792dd26d70050b2f7fab02be9ea33e52c83a2be73a2700";
+export const validator2Public = SDK.util.getPublicFromPrivate(validator2Secret);
+// db3a858d2bafd2cb5382fcf366b847a86b58b42ce1fc29fec0cb0315af881a2ad495045adbdbc86ef7a777b541c4e62a0747f25ff6068a5ec3a052c690c4ff8a
+export const validator2AccountId = SDK.util.getAccountIdFromPrivate(
+    validator2Secret
+); // d32d7cd32af1703400c9624ea3ba488d7a0e6d17
+export const validator2Address = SDK.Core.classes.PlatformAddress.fromAccountId(
+    validator2AccountId,
+    { networkId: "tc" }
+); // tccq8fj6lxn9tchqdqqe93yaga6fzxh5rndzu8k2gdw
+
+export const validator3Secret =
+    "0afa81c02fba3671ec9578f3be040e0186b445e9dc37d8bf4a866c8636841836";
+export const validator3Public = SDK.util.getPublicFromPrivate(validator3Secret);
+// 42829b18de338aa3abf5e6d80cd511121bf9d34be9a135bbace32a3226479e7f3bb6af76c11dcc724a1666a22910d756b075d54d8fdd97be11efd7a0ac3bb222
+export const validator3AccountId = SDK.util.getAccountIdFromPrivate(
+    validator3Secret
+); // 49acbedaea4afa1c00adea94856536fab532d927
+export const validator3Address = SDK.Core.classes.PlatformAddress.fromAccountId(
+    validator3AccountId,
+    { networkId: "tc" }
+); // tccq9y6e0k6af9058qq4h4ffpt9xmat2vkeyue23j8y
