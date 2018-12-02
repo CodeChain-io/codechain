93e10f3f3358c16f6477f88b68b3d8cecc36368b
Seulgi Kim <seulgi.kim@kodebox.io>
2018-12-02 17:55:32 +0900

    No need to create string to compare Ident

--- util/rlp_derive/src/de.rs
+++ util/rlp_derive/src/de.rs
@@ -120,7 +120,7 @@ fn decodable_field(index: usize, field: &syn::Field, quotes: ParseQuotes) -> quo
     match field.ty {
         syn::Ty::Path(_, ref path) => {
             let ident = &path.segments.first().expect("there must be at least 1 segment").ident;
-            if &ident.to_string() == "Vec" {
+            if ident == "Vec" {
                 if quotes.takes_index {
                     quote! { #id: #list(#index)?, }
                 } else {
