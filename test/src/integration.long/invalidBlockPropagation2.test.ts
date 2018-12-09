1db51536d7ea12db354549a0c5cdf14b88869dd8
Seulgi Kim <seulgi.kim@kodebox.io>
2018-12-09 17:22:04 +0900

    Seperate tests for invalid block propagation
    Because Travis halts if there is no stdout over 10 minutes and
    mocha-parallel-tests don't print stdout until the tests in a file
    finished, the Travis failed even there is no error.

---
+++ test/src/integration.long/invalidBlockPropagation2.test.ts
@@ -0,0 +1,27 @@
+// Copyright 2018 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import { U256 } from "codechain-primitives/lib";
+
+import { expect } from "chai";
+import "mocha";
+import { createTestSuite } from "./invalidBlockPropagation.helper";
+
+const INVALID_NUMBER = new U256(2);
+const params = {
+    tnumber: INVALID_NUMBER
+};
+createTestSuite(2, "OnChain invalid number block propagation test", params);
---
+++ test/src/integration.long/invalidBlockPropagation6.test.ts
@@ -0,0 +1,29 @@
+// Copyright 2018 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import { H256 } from "codechain-primitives/lib";
+
+import { expect } from "chai";
+import "mocha";
+import { createTestSuite } from "./invalidBlockPropagation.helper";
+
+const INVALID_STATEROOT = new H256(
+    "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+);
+const params = {
+    tstateRoot: INVALID_STATEROOT
+};
+createTestSuite(6, "OnChain invalid stateRoot block propagation test", params);
