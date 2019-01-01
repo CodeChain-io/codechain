52e4445ae342a1c6bda041d9ce8e4a647cbf287b
SeongChan Lee <sclee@kodebox.io>
2019-01-01 12:50:11 +0900

    Add engine_getCustomActionData, ActionHandler test

--- test/src/integration/engine.test.ts
+++ test/src/integration/engine.test.ts
@@ -18,6 +18,16 @@ import CodeChain from "../helper/spawn";
 
 import "mocha";
 import { expect } from "chai";
+import {
+    hitActionHandlerId,
+    faucetSecret,
+    faucetAddress
+} from "../helper/constants";
+import { toHex } from "codechain-primitives/lib";
+import { fail } from "assert";
+import { errorMatcher, ERROR } from "../helper/error";
+
+const RLP = require("rlp");
 
 describe("engine", function() {
     let node: CodeChain;
@@ -41,6 +51,79 @@ describe("engine", function() {
                 []
             )
         ).to.equal(1);
+    });
+
+    describe("getCustomActionData", function() {
+        it("should have initial state", async function() {
+            const actionData = await node.sdk.rpc.engine.getCustomActionData(
+                hitActionHandlerId,
+                ["metadata hit"]
+            );
+
+            expect(actionData).to.be.equal(toHex(RLP.encode(1)));
+        });
+
+        it("should alter state", async function() {
+            const hash = await node.sdk.rpc.chain.sendSignedParcel(
+                node.sdk.core
+                    .createCustomParcel({
+                        handlerId: hitActionHandlerId,
+                        bytes: RLP.encode([11])
+                    })
+                    .sign({
+                        secret: faucetSecret,
+                        seq: await node.sdk.rpc.chain.getSeq(faucetAddress),
+                        fee: 10
+                    })
+            );
+
+            const invoice = (await node.sdk.rpc.chain.getParcelInvoice(hash, {
+                timeout: 120 * 1000
+            }))!;
+
+            expect(invoice.error).to.be.undefined;
+            expect(invoice.success).to.be.true;
+
+            const actionData = await node.sdk.rpc.engine.getCustomActionData(
+                hitActionHandlerId,
+                ["metadata hit"]
+            );
+
+            expect(actionData).to.be.equal(toHex(RLP.encode(12)));
+        });
+
+        it("should return null", async function() {
+            const actionData = await node.sdk.rpc.engine.getCustomActionData(
+                hitActionHandlerId,
+                ["non-existing-key"]
+            );
+
+            expect(actionData).to.be.null;
+        });
+
+        it("should throw state not exist", async function() {
+            try {
+                await node.sdk.rpc.engine.getCustomActionData(
+                    hitActionHandlerId,
+                    ["metadata hit"],
+                    99999
+                );
+                fail();
+            } catch (e) {
+                expect(e).to.satisfy(errorMatcher(ERROR.STATE_NOT_EXIST));
+            }
+        });
+
+        it("should throw hander not found", async function() {
+            try {
+                await node.sdk.rpc.engine.getCustomActionData(999999, []);
+                fail();
+            } catch (e) {
+                expect(e).to.satisfy(
+                    errorMatcher(ERROR.ACTION_DATA_HANDLER_NOT_FOUND)
+                );
+            }
+        });
     });
 
     afterEach(function() {
