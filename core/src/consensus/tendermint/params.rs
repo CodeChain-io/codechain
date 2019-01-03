3c6837ae849d817e8f241372a311ce6fa9c188e8
SeongChan Lee <sclee@kodebox.io>
2019-01-03 19:29:46 +0900

    Wait more time if Tendermint's current round is increased

--- core/src/consensus/tendermint/params.rs
+++ core/src/consensus/tendermint/params.rs
@@ -20,14 +20,15 @@ use std::collections::HashMap;
 use time::Duration;
 
 use super::super::validator_set::{new_validator_set, ValidatorSet};
-use super::{Step, Timeouts};
+use super::types::View;
+use super::Step;
 
 /// `Tendermint` params.
 pub struct TendermintParams {
     /// List of validators.
     pub validators: Box<ValidatorSet>,
     /// Timeout durations for different steps.
-    pub timeouts: TendermintTimeouts,
+    pub timeouts: TimeoutParams,
     /// Reward per block in base units.
     pub block_reward: u64,
     /// Tokens distributed at genesis.
@@ -36,13 +37,16 @@ pub struct TendermintParams {
 
 impl From<cjson::scheme::TendermintParams> for TendermintParams {
     fn from(p: cjson::scheme::TendermintParams) -> Self {
-        let dt = TendermintTimeouts::default();
+        let dt = TimeoutParams::default();
         TendermintParams {
             validators: new_validator_set(p.validators),
-            timeouts: TendermintTimeouts {
+            timeouts: TimeoutParams {
                 propose: p.timeout_propose.map_or(dt.propose, to_duration),
+                propose_delta: p.timeout_propose_delta.map_or(dt.propose_delta, to_duration),
                 prevote: p.timeout_prevote.map_or(dt.prevote, to_duration),
+                prevote_delta: p.timeout_prevote_delta.map_or(dt.prevote_delta, to_duration),
                 precommit: p.timeout_precommit.map_or(dt.precommit, to_duration),
+                precommit_delta: p.timeout_precommit_delta.map_or(dt.precommit_delta, to_duration),
                 commit: p.timeout_commit.map_or(dt.commit, to_duration),
             },
             block_reward: p.block_reward.map_or(0, Into::into),
@@ -63,35 +67,48 @@ fn to_duration(ms: cjson::uint::Uint) -> Duration {
 
 /// Base timeout of each step in ms.
 #[derive(Debug, Clone)]
-pub struct TendermintTimeouts {
+pub struct TimeoutParams {
     pub propose: Duration,
+    pub propose_delta: Duration,
     pub prevote: Duration,
+    pub prevote_delta: Duration,
     pub precommit: Duration,
+    pub precommit_delta: Duration,
     pub commit: Duration,
 }
 
-impl Default for TendermintTimeouts {
+impl Default for TimeoutParams {
     fn default() -> Self {
-        TendermintTimeouts {
+        TimeoutParams {
             propose: Duration::milliseconds(1000),
+            propose_delta: Duration::milliseconds(500),
             prevote: Duration::milliseconds(1000),
+            prevote_delta: Duration::milliseconds(500),
             precommit: Duration::milliseconds(1000),
+            precommit_delta: Duration::milliseconds(500),
             commit: Duration::milliseconds(1000),
         }
     }
 }
 
-impl Timeouts<Step> for TendermintTimeouts {
-    fn initial(&self) -> Duration {
+impl TimeoutParams {
+    pub fn initial(&self) -> Duration {
         self.propose
     }
 
-    fn timeout(&self, step: &Step) -> Duration {
-        match *step {
+    pub fn timeout(&self, step: Step, view: View) -> Duration {
+        let base = match step {
             Step::Propose => self.propose,
             Step::Prevote => self.prevote,
             Step::Precommit => self.precommit,
             Step::Commit => self.commit,
-        }
+        };
+        let delta = match step {
+            Step::Propose => self.propose_delta,
+            Step::Prevote => self.prevote_delta,
+            Step::Precommit => self.precommit_delta,
+            Step::Commit => Duration::zero(),
+        };
+        base + delta * view as i32
     }
 }
