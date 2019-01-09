2abbb5d9966fc67bbd305196a0839aaeab259b48
Hyunsik Jeong <rbtree@kodebox.io>
2019-01-09 15:22:28 +0900

    Remove LocalTransactionsList from MemPool

--- core/src/miner/mod.rs
+++ core/src/miner/mod.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-mod local_tranasctions;
 mod mem_pool;
 #[cfg_attr(feature = "cargo-clippy", allow(clippy::module_inception))]
 mod miner;
