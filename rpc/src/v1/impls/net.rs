d3f44c4beb2c9a86d820be9087f82f4ab5a0f9dd
Seulgi Kim <seulgi.kim@kodebox.io>
2019-01-28 13:15:02 +0900

    Import module to avoid repetitions

--- rpc/src/v1/impls/net.rs
+++ rpc/src/v1/impls/net.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use std::net::{self, IpAddr};
 use std::sync::Arc;
 
 use cnetwork::{NetworkControl, SocketAddr};
@@ -36,17 +37,17 @@ impl NetClient {
 }
 
 impl Net for NetClient {
-    fn connect(&self, address: ::std::net::IpAddr, port: u16) -> Result<()> {
+    fn connect(&self, address: IpAddr, port: u16) -> Result<()> {
         self.network_control.connect(SocketAddr::new(address, port)).map_err(|e| errors::network_control(&e))?;
         Ok(())
     }
 
-    fn disconnect(&self, address: ::std::net::IpAddr, port: u16) -> Result<()> {
+    fn disconnect(&self, address: IpAddr, port: u16) -> Result<()> {
         self.network_control.disconnect(SocketAddr::new(address, port)).map_err(|e| errors::network_control(&e))?;
         Ok(())
     }
 
-    fn is_connected(&self, address: ::std::net::IpAddr, port: u16) -> Result<bool> {
+    fn is_connected(&self, address: IpAddr, port: u16) -> Result<bool> {
         Ok(self
             .network_control
             .is_connected(&SocketAddr::new(address, port))
@@ -61,24 +62,24 @@ impl Net for NetClient {
         Ok(self.network_control.get_peer_count().map_err(|e| errors::network_control(&e))?)
     }
 
-    fn get_established_peers(&self) -> Result<Vec<::std::net::SocketAddr>> {
+    fn get_established_peers(&self) -> Result<Vec<net::SocketAddr>> {
         let peers = self.network_control.established_peers().map_err(|e| errors::network_control(&e))?;
         Ok(peers.into_iter().map(Into::into).collect())
     }
 
-    fn add_to_whitelist(&self, addr: ::std::net::IpAddr, tag: Option<String>) -> Result<()> {
+    fn add_to_whitelist(&self, addr: IpAddr, tag: Option<String>) -> Result<()> {
         self.network_control.add_to_whitelist(addr, tag).map_err(|e| errors::network_control(&e))
     }
 
-    fn remove_from_whitelist(&self, addr: ::std::net::IpAddr) -> Result<()> {
+    fn remove_from_whitelist(&self, addr: IpAddr) -> Result<()> {
         self.network_control.remove_from_whitelist(&addr).map_err(|e| errors::network_control(&e))
     }
 
-    fn add_to_blacklist(&self, addr: ::std::net::IpAddr, tag: Option<String>) -> Result<()> {
+    fn add_to_blacklist(&self, addr: IpAddr, tag: Option<String>) -> Result<()> {
         self.network_control.add_to_blacklist(addr, tag).map_err(|e| errors::network_control(&e))
     }
 
-    fn remove_from_blacklist(&self, addr: ::std::net::IpAddr) -> Result<()> {
+    fn remove_from_blacklist(&self, addr: IpAddr) -> Result<()> {
         self.network_control.remove_from_blacklist(&addr).map_err(|e| errors::network_control(&e))
     }
 
