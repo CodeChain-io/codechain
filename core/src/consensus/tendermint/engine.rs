e7e55c40e664a81acd5b94337d2de1a918d9790f
Seulgi Kim <seulgi.kim@kodebox.io>
2019-03-18 18:14:49 +0900

    Split the Tendermint module into several files

---
+++ core/src/consensus/tendermint/engine.rs
@@ -0,0 +1,299 @@
+// Copyright 2018-2019 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::iter::Iterator;
+use std::sync::atomic::Ordering as AtomicOrdering;
+use std::sync::{Arc, Weak};
+
+use ckey::{public_to_address, recover_schnorr, Address, Message, SchnorrSignature};
+use cnetwork::NetworkService;
+use crossbeam_channel as crossbeam;
+use cstate::ActionHandler;
+use ctypes::machine::WithBalances;
+use ctypes::BlockNumber;
+use primitives::H256;
+use rlp::UntrustedRlp;
+
+use super::super::{ConsensusEngine, ConstructedVerifier, EngineError, EpochChange, Seal};
+use super::epoch_verifier::EpochVerifier;
+use super::network::TendermintExtension;
+pub use super::params::{TendermintParams, TimeoutParams};
+use super::worker;
+use super::{stake, ChainNotify, Tendermint, SEAL_FIELDS};
+use crate::account_provider::AccountProvider;
+use crate::block::*;
+use crate::client::{Client, EngineClient};
+use crate::codechain_machine::CodeChainMachine;
+use crate::consensus::EngineType;
+use crate::error::Error;
+use crate::header::Header;
+use crate::views::HeaderView;
+
+impl ConsensusEngine<CodeChainMachine> for Tendermint {
+    fn name(&self) -> &str {
+        "Tendermint"
+    }
+
+    fn machine(&self) -> &CodeChainMachine {
+        &self.machine.as_ref()
+    }
+
+    /// (consensus view, proposal signature, authority signatures)
+    fn seal_fields(&self, _header: &Header) -> usize {
+        SEAL_FIELDS
+    }
+
+    /// Should this node participate.
+    fn seals_internally(&self) -> Option<bool> {
+        Some(self.has_signer.load(AtomicOrdering::SeqCst))
+    }
+
+    fn engine_type(&self) -> EngineType {
+        EngineType::PBFT
+    }
+
+    /// Attempt to seal generate a proposal seal.
+    ///
+    /// This operation is synchronous and may (quite reasonably) not be available, in which case
+    /// `Seal::None` will be returned.
+    fn generate_seal(&self, block: &ExecutedBlock, parent: &Header) -> Seal {
+        let (result, receiver) = crossbeam::bounded(1);
+        let block_number = block.header().number();
+        let parent_hash = parent.hash();
+        self.inner
+            .send(worker::Event::GenerateSeal {
+                block_number,
+                parent_hash,
+                result,
+            })
+            .unwrap();
+        receiver.recv().unwrap()
+    }
+
+    /// Called when the node is the leader and a proposal block is generated from the miner.
+    /// This writes the proposal information and go to the prevote step.
+    fn proposal_generated(&self, sealed_block: &SealedBlock) {
+        self.inner.send(worker::Event::ProposalGenerated(Box::from(sealed_block.clone()))).unwrap();
+    }
+
+    fn verify_local_seal(&self, _header: &Header) -> Result<(), Error> {
+        Ok(())
+    }
+
+    fn verify_block_basic(&self, header: &Header) -> Result<(), Error> {
+        let (result, receiver) = crossbeam::bounded(1);
+        self.inner
+            .send(worker::Event::VerifyBlockBasic {
+                header: Box::from(header.clone()),
+                result,
+            })
+            .unwrap();
+        receiver.recv().unwrap()
+    }
+
+    fn verify_block_external(&self, header: &Header) -> Result<(), Error> {
+        let (result, receiver) = crossbeam::bounded(1);
+        self.inner
+            .send(worker::Event::VerifyBlockExternal {
+                header: Box::from(header.clone()),
+                result,
+            })
+            .unwrap();
+        receiver.recv().unwrap()
+    }
+
+    fn signals_epoch_end(&self, header: &Header) -> EpochChange {
+        let first = header.number() == 0;
+        self.validators.signals_epoch_end(first, header)
+    }
+
+    fn is_epoch_end(
+        &self,
+        chain_head: &Header,
+        _chain: &super::super::Headers<Header>,
+        transition_store: &super::super::PendingTransitionStore,
+    ) -> Option<Vec<u8>> {
+        let first = chain_head.number() == 0;
+
+        if let Some(change) = self.validators.is_epoch_end(first, chain_head) {
+            let change = combine_proofs(chain_head.number(), &change, &[]);
+            return Some(change)
+        } else if let Some(pending) = transition_store(chain_head.hash()) {
+            let signal_number = chain_head.number();
+            let finality_proof = ::rlp::encode(chain_head);
+            return Some(combine_proofs(signal_number, &pending.proof, &finality_proof))
+        }
+
+        None
+    }
+
+    fn epoch_verifier<'a>(&self, _header: &Header, proof: &'a [u8]) -> ConstructedVerifier<'a, CodeChainMachine> {
+        let (signal_number, set_proof, finality_proof) = match destructure_proofs(proof) {
+            Ok(x) => x,
+            Err(e) => return ConstructedVerifier::Err(e),
+        };
+
+        let first = signal_number == 0;
+        match self.validators.epoch_set(first, &self.machine, signal_number, set_proof) {
+            Ok((list, finalize)) => {
+                let verifier = Box::new(EpochVerifier::new(list, |signature: &SchnorrSignature, message: &Message| {
+                    Ok(public_to_address(&recover_schnorr(&signature, &message)?))
+                }));
+
+                match finalize {
+                    Some(finalize) => ConstructedVerifier::Unconfirmed(verifier, finality_proof, finalize),
+                    None => ConstructedVerifier::Trusted(verifier),
+                }
+            }
+            Err(e) => ConstructedVerifier::Err(e),
+        }
+    }
+
+    fn populate_from_parent(&self, header: &mut Header, _parent: &Header) {
+        let (result, receiver) = crossbeam::bounded(1);
+        self.inner
+            .send(worker::Event::CalculateScore {
+                block_number: header.number(),
+                result,
+            })
+            .unwrap();
+        let score = receiver.recv().unwrap();
+        header.set_score(score);
+    }
+
+    /// Equivalent to a timeout: to be used for tests.
+    fn on_timeout(&self, token: usize) {
+        self.inner.send(worker::Event::OnTimeout(token)).unwrap();
+    }
+
+    fn stop(&self) {}
+
+    fn on_new_block(&self, block: &mut ExecutedBlock, epoch_begin: bool) -> Result<(), Error> {
+        let (result, receiver) = crossbeam::bounded(1);
+        self.inner
+            .send(worker::Event::OnNewBlock {
+                header: Box::from(block.header().clone()),
+                epoch_begin,
+                result,
+            })
+            .unwrap();
+        receiver.recv().unwrap()
+    }
+
+    fn on_close_block(&self, block: &mut ExecutedBlock) -> Result<(), Error> {
+        let author = *block.header().author();
+        let transactions = block.transactions().to_owned().into_iter();
+        let fee = transactions.map(|tx| tx.fee).sum();
+        let stakes = stake::get_stakes(block.state()).expect("Cannot get Stake status");
+
+        for (address, share) in stake::fee_distribute(&author, fee, &stakes) {
+            self.machine.add_balance(block, &address, share)?
+        }
+        Ok(())
+    }
+
+    fn register_client(&self, client: Weak<EngineClient>) {
+        *self.client.write() = Some(Weak::clone(&client));
+    }
+
+    fn handle_message(&self, rlp: &[u8]) -> Result<(), EngineError> {
+        let (result, receiver) = crossbeam::bounded(1);
+        self.inner
+            .send(worker::Event::HandleMessage {
+                message: rlp.to_owned(),
+                result,
+            })
+            .unwrap();
+        receiver.recv().unwrap()
+    }
+
+    fn is_proposal(&self, header: &Header) -> bool {
+        let (result, receiver) = crossbeam::bounded(1);
+        self.inner
+            .send(worker::Event::IsProposal {
+                block_number: header.number(),
+                block_hash: header.hash(),
+                result,
+            })
+            .unwrap();
+        receiver.recv().unwrap()
+    }
+
+    fn set_signer(&self, ap: Arc<AccountProvider>, address: Address) {
+        self.has_signer.store(true, AtomicOrdering::SeqCst);
+        self.inner
+            .send(worker::Event::SetSigner {
+                ap,
+                address,
+            })
+            .unwrap();
+    }
+
+    fn register_network_extension_to_service(&self, service: &NetworkService) {
+        let timeouts = self.timeouts;
+
+        let inner = self.inner.clone();
+        let extension = service.register_extension(move |api| TendermintExtension::new(inner, timeouts, api));
+        let client = Weak::clone(self.client.read().as_ref().unwrap());
+        self.extension_initializer.send((extension, client)).unwrap();
+
+        let (result, receiver) = crossbeam::bounded(1);
+        self.inner.send(worker::Event::Restore(result)).unwrap();
+        receiver.recv().unwrap();
+    }
+
+    fn block_reward(&self, _block_number: u64) -> u64 {
+        self.block_reward
+    }
+
+    fn recommended_confirmation(&self) -> u32 {
+        1
+    }
+
+    fn register_chain_notify(&self, client: &Client) {
+        client.add_notify(Arc::downgrade(&self.chain_notify) as Weak<ChainNotify>);
+    }
+
+    fn get_best_block_from_best_proposal_header(&self, header: &HeaderView) -> H256 {
+        header.parent_hash()
+    }
+
+    fn can_change_canon_chain(&self, header: &HeaderView) -> bool {
+        let (result, receiver) = crossbeam::bounded(1);
+        self.inner
+            .send(worker::Event::AllowedHeight {
+                result,
+            })
+            .unwrap();
+        let allowed_height = receiver.recv().unwrap();
+        header.number() >= allowed_height
+    }
+
+    fn action_handlers(&self) -> &[Arc<ActionHandler>] {
+        &self.action_handlers
+    }
+}
+
+fn combine_proofs(signal_number: BlockNumber, set_proof: &[u8], finality_proof: &[u8]) -> Vec<u8> {
+    let mut stream = ::rlp::RlpStream::new_list(3);
+    stream.append(&signal_number).append(&set_proof).append(&finality_proof);
+    stream.out()
+}
+
+fn destructure_proofs(combined: &[u8]) -> Result<(BlockNumber, &[u8], &[u8]), Error> {
+    let rlp = UntrustedRlp::new(combined);
+    Ok((rlp.at(0)?.as_val()?, rlp.at(1)?.data()?, rlp.at(2)?.data()?))
+}
---
+++ core/src/consensus/tendermint/network.rs
@@ -0,0 +1,433 @@
+// Copyright 2018-2019 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use std::cmp;
+use std::collections::HashMap;
+use std::iter::Iterator;
+
+use ckey::SchnorrSignature;
+use cnetwork::{Api, NetworkExtension, NodeId};
+use crossbeam_channel as crossbeam;
+use ctimer::TimerToken;
+use primitives::{Bytes, H256};
+use rand::prelude::SliceRandom;
+use rand::thread_rng;
+use rlp::{Encodable, UntrustedRlp};
+use time::Duration;
+
+use super::message::*;
+use super::params::TimeoutParams;
+use super::types::{BitSet, Height, PeerState, Step, View};
+use super::worker;
+use crate::consensus::EngineError;
+
+use super::{
+    ENGINE_TIMEOUT_BROADCAST_STEP_STATE, ENGINE_TIMEOUT_BROADCAT_STEP_STATE_INTERVAL, ENGINE_TIMEOUT_EMPTY_PROPOSAL,
+    ENGINE_TIMEOUT_TOKEN_NONCE_BASE,
+};
+
+pub struct TendermintExtension {
+    inner: crossbeam::Sender<worker::Event>,
+    peers: HashMap<NodeId, PeerState>,
+    api: Box<Api>,
+    timeouts: TimeoutParams,
+}
+
+const MIN_PEERS_PROPAGATION: usize = 4;
+const MAX_PEERS_PROPAGATION: usize = 128;
+
+impl TendermintExtension {
+    pub fn new(inner: crossbeam::Sender<worker::Event>, timeouts: TimeoutParams, api: Box<Api>) -> Self {
+        let initial = timeouts.initial();
+        ctrace!(ENGINE, "Setting the initial timeout to {}.", initial);
+        api.set_timer_once(ENGINE_TIMEOUT_TOKEN_NONCE_BASE, initial).expect("Timer set succeeds");
+        api.set_timer(
+            ENGINE_TIMEOUT_BROADCAST_STEP_STATE,
+            Duration::seconds(ENGINE_TIMEOUT_BROADCAT_STEP_STATE_INTERVAL),
+        )
+        .expect("Timer set succeeds");
+        Self {
+            inner,
+            peers: Default::default(),
+            api,
+            timeouts,
+        }
+    }
+
+    fn update_peer_state(&mut self, token: &NodeId, vote_step: VoteStep, proposal: Option<H256>, messages: BitSet) {
+        let peer_state = match self.peers.get_mut(token) {
+            Some(peer_state) => peer_state,
+            // update_peer_state could be called after the peer is disconnected
+            None => return,
+        };
+        peer_state.vote_step = vote_step;
+        peer_state.proposal = proposal;
+        peer_state.messages = messages;
+    }
+
+    fn select_random_peers(&self) -> Vec<NodeId> {
+        let mut peers: Vec<NodeId> = self.peers.keys().cloned().collect();
+        let mut count = (peers.len() as f64).powf(0.5).round() as usize;
+        count = cmp::min(count, MAX_PEERS_PROPAGATION);
+        count = cmp::max(count, MIN_PEERS_PROPAGATION);
+        peers.shuffle(&mut thread_rng());
+        peers.truncate(count);
+        peers
+    }
+
+    fn broadcast_message(&self, message: Bytes) {
+        let tokens = self.select_random_peers();
+        let message = TendermintMessage::ConsensusMessage(message).rlp_bytes().into_vec();
+        for token in tokens {
+            self.api.send(&token, &message);
+        }
+    }
+
+    fn send_message(&self, token: &NodeId, message: Bytes) {
+        ctrace!(ENGINE, "Send message to {}", token);
+        let message = TendermintMessage::ConsensusMessage(message).rlp_bytes().into_vec();
+        self.api.send(token, &message);
+    }
+
+    fn broadcast_state(&self, vote_step: VoteStep, proposal: Option<H256>, lock_view: Option<View>, votes: BitSet) {
+        ctrace!(ENGINE, "Broadcast state {:?} {:?} {:?}", vote_step, proposal, votes);
+        let tokens = self.select_random_peers();
+        let message = TendermintMessage::StepState {
+            vote_step,
+            proposal,
+            lock_view,
+            known_votes: votes,
+        }
+        .rlp_bytes()
+        .into_vec();
+
+        for token in tokens {
+            self.api.send(&token, &message);
+        }
+    }
+
+    fn broadcast_proposal_block(&self, signature: SchnorrSignature, view: View, message: Bytes) {
+        let message = TendermintMessage::ProposalBlock {
+            signature,
+            message,
+            view,
+        }
+        .rlp_bytes()
+        .into_vec();
+        for token in self.peers.keys() {
+            self.api.send(token, &message);
+        }
+    }
+
+    fn request_proposal_to_any(&self, height: Height, view: View) {
+        for (token, peer) in &self.peers {
+            let is_future_height_and_view = {
+                let higher_height = peer.vote_step.height > height;
+                let same_height_and_higher_view = peer.vote_step.height == height && peer.vote_step.view > view;
+                higher_height || same_height_and_higher_view
+            };
+
+            if is_future_height_and_view {
+                self.request_proposal(token, height, view);
+                continue
+            }
+
+            let is_same_height_and_view = peer.vote_step.height == height && peer.vote_step.view == view;
+
+            if is_same_height_and_view && peer.proposal.is_some() {
+                self.request_proposal(token, height, view);
+            }
+        }
+    }
+
+    fn request_proposal(&self, token: &NodeId, height: Height, view: View) {
+        ctrace!(ENGINE, "Request proposal {} {} to {:?}", height, view, token);
+        let message = TendermintMessage::RequestProposal {
+            height,
+            view,
+        }
+        .rlp_bytes()
+        .into_vec();
+        self.api.send(&token, &message);
+    }
+
+    fn request_messages_to_all(&self, vote_step: VoteStep, requested_votes: BitSet) {
+        for token in self.select_random_peers() {
+            let peer = &self.peers[&token];
+            if vote_step <= peer.vote_step && !peer.messages.is_empty() {
+                self.request_messages(&token, vote_step, requested_votes);
+            }
+        }
+    }
+
+    fn request_messages(&self, token: &NodeId, vote_step: VoteStep, requested_votes: BitSet) {
+        ctrace!(ENGINE, "Request messages {:?} {:?} to {:?}", vote_step, requested_votes, token);
+        let message = TendermintMessage::RequestMessage {
+            vote_step,
+            requested_votes,
+        }
+        .rlp_bytes()
+        .into_vec();
+        self.api.send(&token, &message);
+    }
+
+    fn set_timer_step(&self, step: Step, view: View, expired_token_nonce: TimerToken) {
+        self.api.clear_timer(ENGINE_TIMEOUT_EMPTY_PROPOSAL).expect("Timer clear succeeds");
+        self.api.clear_timer(expired_token_nonce).expect("Timer clear succeeds");
+        self.api
+            .set_timer_once(expired_token_nonce + 1, self.timeouts.timeout(step, view))
+            .expect("Timer set succeeds");
+    }
+
+    fn set_timer_empty_proposal(&self, view: View) {
+        self.api.clear_timer(ENGINE_TIMEOUT_EMPTY_PROPOSAL).expect("Timer clear succeeds");
+        self.api
+            .set_timer_once(ENGINE_TIMEOUT_EMPTY_PROPOSAL, self.timeouts.timeout(Step::Propose, view) / 2)
+            .expect("Timer set succeeds");
+    }
+}
+
+impl NetworkExtension<Event> for TendermintExtension {
+    fn name() -> &'static str {
+        "tendermint"
+    }
+
+    fn need_encryption() -> bool {
+        false
+    }
+
+    fn versions() -> &'static [u64] {
+        const VERSIONS: &[u64] = &[0];
+        &VERSIONS
+    }
+
+    fn on_node_added(&mut self, token: &NodeId, _version: u64) {
+        self.peers.insert(*token, PeerState::new());
+    }
+
+    fn on_node_removed(&mut self, token: &NodeId) {
+        self.peers.remove(token);
+    }
+
+    fn on_message(&mut self, token: &NodeId, data: &[u8]) {
+        let m = UntrustedRlp::new(data);
+        match m.as_val() {
+            Ok(TendermintMessage::ConsensusMessage(ref bytes)) => {
+                let (result, receiver) = crossbeam::bounded(1);
+                self.inner
+                    .send(worker::Event::HandleMessage {
+                        message: bytes.clone(),
+                        result,
+                    })
+                    .unwrap();
+                match receiver.recv().unwrap() {
+                    Err(EngineError::FutureMessage {
+                        future_height,
+                        current_height,
+                    }) => {
+                        cdebug!(
+                            ENGINE,
+                            "Could not handle future message from {}, in height {}",
+                            future_height,
+                            current_height
+                        );
+                    }
+                    Err(e) => {
+                        cinfo!(ENGINE, "Failed to handle message {:?}", e);
+                    }
+                    Ok(_) => {}
+                }
+            }
+            Ok(TendermintMessage::ProposalBlock {
+                signature,
+                view,
+                message,
+            }) => {
+                let (result, receiver) = crossbeam::bounded(1);
+                self.inner
+                    .send(worker::Event::ProposalBlock {
+                        signature,
+                        view,
+                        message: message.clone(),
+                        result,
+                    })
+                    .unwrap();
+                if let Some(c) = receiver.recv().unwrap() {
+                    if let Err(e) = c.import_block(message) {
+                        cinfo!(ENGINE, "Failed to import proposal block {:?}", e);
+                    }
+                }
+            }
+            Ok(TendermintMessage::StepState {
+                vote_step,
+                proposal,
+                lock_view,
+                known_votes,
+            }) => {
+                ctrace!(
+                    ENGINE,
+                    "Peer state update step: {:?} proposal {:?} peer_lock_view {:?} known_votes {:?}",
+                    vote_step,
+                    proposal,
+                    lock_view,
+                    known_votes,
+                );
+                self.update_peer_state(token, vote_step, proposal, known_votes);
+                let (result, receiver) = crossbeam::unbounded();
+                self.inner
+                    .send(worker::Event::StepState {
+                        token: *token,
+                        vote_step,
+                        proposal,
+                        lock_view,
+                        known_votes: Box::from(known_votes),
+                        result,
+                    })
+                    .unwrap();
+
+                while let Ok(message) = receiver.recv() {
+                    self.api.send(token, &message);
+                }
+            }
+            Ok(TendermintMessage::RequestProposal {
+                height,
+                view,
+            }) => {
+                let (result, receiver) = crossbeam::bounded(1);
+                self.inner
+                    .send(worker::Event::RequestProposal {
+                        token: *token,
+                        height,
+                        view,
+                        result,
+                    })
+                    .unwrap();
+                if let Ok(message) = receiver.recv() {
+                    self.api.send(token, &message);
+                }
+            }
+            Ok(TendermintMessage::RequestMessage {
+                vote_step,
+                requested_votes,
+            }) => {
+                ctrace!(ENGINE, "Received RequestMessage for {:?} from {:?}", vote_step, requested_votes);
+
+                let (result, receiver) = crossbeam::unbounded();
+                self.inner
+                    .send(worker::Event::GetAllVotesAndAuthors {
+                        vote_step,
+                        requested: requested_votes,
+                        result,
+                    })
+                    .unwrap();
+
+                for vote in receiver.iter() {
+                    self.send_message(token, vote.rlp_bytes().into_vec());
+                }
+            }
+            _ => cinfo!(ENGINE, "Invalid message from peer {}", token),
+        }
+    }
+
+    fn on_timeout(&mut self, token: TimerToken) {
+        debug_assert!(
+            token >= ENGINE_TIMEOUT_TOKEN_NONCE_BASE
+                || token == ENGINE_TIMEOUT_EMPTY_PROPOSAL
+                || token == ENGINE_TIMEOUT_BROADCAST_STEP_STATE
+        );
+        self.inner.send(worker::Event::OnTimeout(token)).unwrap();
+    }
+
+    fn on_event(&mut self, event: Event) {
+        match event {
+            Event::BroadcastMessage {
+                message,
+            } => {
+                self.broadcast_message(message);
+            }
+            Event::BroadcastState {
+                vote_step,
+                proposal,
+                lock_view,
+                votes,
+            } => {
+                self.broadcast_state(vote_step, proposal, lock_view, votes);
+            }
+            Event::RequestMessagesToAll {
+                vote_step,
+                requested_votes,
+            } => {
+                self.request_messages_to_all(vote_step, requested_votes);
+            }
+            Event::RequestProposalToAny {
+                height,
+                view,
+            } => {
+                self.request_proposal_to_any(height, view);
+            }
+            Event::SetTimerStep {
+                step,
+                view,
+                expired_token_nonce,
+            } => self.set_timer_step(step, view, expired_token_nonce),
+            Event::SetTimerEmptyProposal {
+                view,
+            } => {
+                self.set_timer_empty_proposal(view);
+            }
+            Event::BroadcastProposalBlock {
+                signature,
+                view,
+                message,
+            } => {
+                self.broadcast_proposal_block(signature, view, message);
+            }
+        }
+    }
+}
+
+pub enum Event {
+    BroadcastMessage {
+        message: Bytes,
+    },
+    BroadcastState {
+        vote_step: VoteStep,
+        proposal: Option<H256>,
+        lock_view: Option<View>,
+        votes: BitSet,
+    },
+    RequestMessagesToAll {
+        vote_step: VoteStep,
+        requested_votes: BitSet,
+    },
+    RequestProposalToAny {
+        height: Height,
+        view: View,
+    },
+    SetTimerStep {
+        step: Step,
+        view: View,
+        expired_token_nonce: TimerToken,
+    },
+    SetTimerEmptyProposal {
+        view: View,
+    },
+    BroadcastProposalBlock {
+        signature: SchnorrSignature,
+        view: View,
+        message: Bytes,
+    },
+}
