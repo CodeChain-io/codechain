a9cd6478b3fce422182aa0294c12d71d18a5bfcd
Seonpyo Kim <tomandjerry@kodebox.io>
2019-04-15 21:21:53 +0900

    Add test cases tied partial signature and multisig together

--- test/src/e2e/multisig.test.ts
+++ test/src/e2e/multisig.test.ts
@@ -17,6 +17,7 @@
 import { Buffer } from "buffer";
 import { expect } from "chai";
 import {
+    Asset,
     AssetAddress,
     H160,
     Script,
@@ -109,16 +110,12 @@ describe("Multisig", function() {
     describe("1 of 2", async function() {
         const lockScript = createLockScript(1, 2, alicePublic, bobPublic);
         const lockScriptHash = new H160(blake160(lockScript));
-
+        let recipient: AssetAddress;
         let transfer: TransferAsset;
         beforeEach(async function() {
-            const recipient = AssetAddress.fromTypeAndPayload(
-                0,
-                lockScriptHash,
-                {
-                    networkId: node.sdk.networkId
-                }
-            );
+            recipient = AssetAddress.fromTypeAndPayload(0, lockScriptHash, {
+                networkId: node.sdk.networkId
+            });
 
             const asset = await node.mintAsset({ supply: 1, recipient });
 
@@ -179,6 +176,149 @@ describe("Multisig", function() {
                 .setUnlockScript(createUnlockScript(defaultTag, signature));
 
             await expectTransactionFail(node, transfer);
+        });
+
+        describe("Test partial signing", async function() {
+            let additionalAsset: Asset;
+            beforeEach(async function() {
+                additionalAsset = await node.mintAsset({
+                    supply: 10,
+                    recipient
+                });
+            });
+            it("unlock with fixed inputs", async function() {
+                const tag: SignatureTag = { input: "all", output: [0] };
+                transfer.addInputs(additionalAsset);
+                transfer.input(1)!.setLockScript(lockScript);
+
+                const hashWithoutScript0 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 0
+                });
+                const signature0 = signEcdsa(
+                    hashWithoutScript0.value,
+                    aliceSecret
+                );
+                const hashWithoutScript1 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 1
+                });
+                const signature1 = signEcdsa(
+                    hashWithoutScript1.value,
+                    bobSecret
+                );
+
+                transfer.addOutputs({
+                    quantity: 10,
+                    assetType: additionalAsset.assetType,
+                    shardId: additionalAsset.shardId,
+                    recipient: await node.createP2PKHAddress()
+                });
+                transfer
+                    .input(0)!
+                    .setUnlockScript(createUnlockScript(tag, signature0));
+
+                transfer
+                    .input(1)!
+                    .setUnlockScript(createUnlockScript(tag, signature1));
+
+                const hash = await node.sendAssetTransaction(transfer);
+                expect(await node.sdk.rpc.chain.containsTransaction(hash)).be
+                    .true;
+                expect(await node.sdk.rpc.chain.getTransaction(hash)).not.null;
+            });
+
+            it("unlock with fixed outputs", async function() {
+                const tag: SignatureTag = { input: "single", output: [0, 1] };
+                transfer.addOutputs({
+                    quantity: 10,
+                    assetType: additionalAsset.assetType,
+                    shardId: additionalAsset.shardId,
+                    recipient: await node.createP2PKHBurnAddress()
+                });
+
+                const hashWithoutScript0 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 0
+                });
+                const signature0 = signEcdsa(
+                    hashWithoutScript0.value,
+                    aliceSecret
+                );
+                transfer.addInputs(additionalAsset);
+                transfer.input(1)!.setLockScript(lockScript);
+
+                const hashWithoutScript1 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 1
+                });
+                const signature1 = signEcdsa(
+                    hashWithoutScript1.value,
+                    bobSecret
+                );
+                transfer
+                    .input(0)!
+                    .setUnlockScript(createUnlockScript(tag, signature0));
+
+                transfer
+                    .input(1)!
+                    .setUnlockScript(createUnlockScript(tag, signature1));
+
+                const hash = await node.sendAssetTransaction(transfer);
+                expect(await node.sdk.rpc.chain.containsTransaction(hash)).be
+                    .true;
+                expect(await node.sdk.rpc.chain.getTransaction(hash)).not.null;
+            });
+
+            it("unlock with both outputs and inputs dynamic", async function() {
+                const tag0: SignatureTag = { input: "single", output: [0] };
+                const tag1: SignatureTag = { input: "single", output: [1] };
+
+                const hashWithoutScript0 = transfer.hashWithoutScript({
+                    tag: tag0,
+                    type: "input",
+                    index: 0
+                });
+                const signature0 = signEcdsa(
+                    hashWithoutScript0.value,
+                    aliceSecret
+                );
+
+                transfer.addInputs(additionalAsset).addOutputs({
+                    quantity: 10,
+                    assetType: additionalAsset.assetType,
+                    shardId: additionalAsset.shardId,
+                    recipient: await node.createP2PKHBurnAddress()
+                });
+                transfer.input(1)!.setLockScript(lockScript);
+
+                const hashWithoutScript1 = transfer.hashWithoutScript({
+                    tag: tag1,
+                    type: "input",
+                    index: 1
+                });
+                const signature1 = signEcdsa(
+                    hashWithoutScript1.value,
+                    bobSecret
+                );
+
+                transfer
+                    .input(0)!
+                    .setUnlockScript(createUnlockScript(tag0, signature0));
+
+                transfer
+                    .input(1)!
+                    .setUnlockScript(createUnlockScript(tag1, signature1));
+
+                const hash = await node.sendAssetTransaction(transfer);
+                expect(await node.sdk.rpc.chain.containsTransaction(hash)).be
+                    .true;
+                expect(await node.sdk.rpc.chain.getTransaction(hash)).not.null;
+            });
         });
     });
 
@@ -295,16 +435,12 @@ describe("Multisig", function() {
             carolPublic
         );
         const lockScriptHash = new H160(blake160(lockScript));
-
+        let recipient: AssetAddress;
         let transfer: TransferAsset;
         beforeEach(async function() {
-            const recipient = AssetAddress.fromTypeAndPayload(
-                0,
-                lockScriptHash,
-                {
-                    networkId: node.sdk.networkId
-                }
-            );
+            recipient = AssetAddress.fromTypeAndPayload(0, lockScriptHash, {
+                networkId: node.sdk.networkId
+            });
 
             const asset = await node.mintAsset({ supply: 1, recipient });
 
@@ -485,6 +621,303 @@ describe("Multisig", function() {
                 );
 
             await expectTransactionFail(node, transfer);
+        });
+
+        describe("Test partial signing", async function() {
+            let additionalAsset: Asset;
+            beforeEach(async function() {
+                additionalAsset = await node.mintAsset({
+                    supply: 10,
+                    recipient
+                });
+            });
+            it("fail to add inputs after sign with all inputs tag", async function() {
+                const tag: SignatureTag = { input: "all", output: [0] };
+                const hashWithoutScript0 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 0
+                });
+                const signature0_1 = signEcdsa(
+                    hashWithoutScript0.value,
+                    aliceSecret
+                );
+                const signature0_2 = signEcdsa(
+                    hashWithoutScript0.value,
+                    bobSecret
+                );
+
+                transfer.addInputs(additionalAsset).addOutputs({
+                    quantity: 10,
+                    assetType: additionalAsset.assetType,
+                    shardId: additionalAsset.shardId,
+                    recipient: await node.createP2PKHAddress()
+                });
+                transfer.input(1)!.setLockScript(lockScript);
+                const hashWithoutScript1 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 1
+                });
+                const signature1_1 = signEcdsa(
+                    hashWithoutScript1.value,
+                    bobSecret
+                );
+                const signature1_2 = signEcdsa(
+                    hashWithoutScript1.value,
+                    carolSecret
+                );
+
+                transfer
+                    .input(0)!
+                    .setUnlockScript(
+                        createUnlockScript(
+                            defaultTag,
+                            signature0_1,
+                            signature0_2
+                        )
+                    );
+
+                transfer
+                    .input(1)!
+                    .setUnlockScript(
+                        createUnlockScript(
+                            defaultTag,
+                            signature1_1,
+                            signature1_2
+                        )
+                    );
+
+                await expectTransactionFail(node, transfer);
+            });
+
+            it("unlock with fixed inputs", async function() {
+                const tag: SignatureTag = { input: "all", output: [0] };
+                transfer.addInputs(additionalAsset);
+                transfer.input(1)!.setLockScript(lockScript);
+
+                const hashWithoutScript0 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 0
+                });
+                const signature0_1 = signEcdsa(
+                    hashWithoutScript0.value,
+                    aliceSecret
+                );
+                const signatrue0_2 = signEcdsa(
+                    hashWithoutScript0.value,
+                    bobSecret
+                );
+
+                const hashWithoutScript1 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 1
+                });
+                const signature1_1 = signEcdsa(
+                    hashWithoutScript1.value,
+                    bobSecret
+                );
+                const signature1_2 = signEcdsa(
+                    hashWithoutScript1.value,
+                    carolSecret
+                );
+
+                transfer.addOutputs({
+                    quantity: 10,
+                    assetType: additionalAsset.assetType,
+                    shardId: additionalAsset.shardId,
+                    recipient: await node.createP2PKHAddress()
+                });
+                transfer
+                    .input(0)!
+                    .setUnlockScript(
+                        createUnlockScript(tag, signature0_1, signatrue0_2)
+                    );
+
+                transfer
+                    .input(1)!
+                    .setUnlockScript(
+                        createUnlockScript(tag, signature1_1, signature1_2)
+                    );
+
+                const hash = await node.sendAssetTransaction(transfer);
+                expect(await node.sdk.rpc.chain.containsTransaction(hash)).be
+                    .true;
+                expect(await node.sdk.rpc.chain.getTransaction(hash)).not.null;
+            });
+
+            it("fail to add inputs after sign with all outputs tag", async function() {
+                const tag: SignatureTag = { input: "single", output: "all" };
+                transfer.addInputs(additionalAsset);
+                transfer.input(1)!.setLockScript(lockScript);
+
+                const hashWithoutScript0 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 0
+                });
+                const signature0_1 = signEcdsa(
+                    hashWithoutScript0.value,
+                    aliceSecret
+                );
+                const signature0_2 = signEcdsa(
+                    hashWithoutScript0.value,
+                    bobSecret
+                );
+
+                transfer.addOutputs({
+                    quantity: 10,
+                    assetType: additionalAsset.assetType,
+                    shardId: additionalAsset.shardId,
+                    recipient: await node.createP2PKHBurnAddress()
+                });
+
+                const hashWithoutScript1 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 1
+                });
+                const signature1_1 = signEcdsa(
+                    hashWithoutScript1.value,
+                    bobSecret
+                );
+                const signature1_2 = signEcdsa(
+                    hashWithoutScript1.value,
+                    carolSecret
+                );
+
+                transfer
+                    .input(0)!
+                    .setUnlockScript(
+                        createUnlockScript(tag, signature0_1, signature0_2)
+                    );
+
+                transfer
+                    .input(1)!
+                    .setUnlockScript(
+                        createUnlockScript(tag, signature1_1, signature1_2)
+                    );
+
+                await expectTransactionFail(node, transfer);
+            });
+
+            it("unlock with fixed outputs", async function() {
+                const tag: SignatureTag = { input: "single", output: "all" };
+                transfer.addOutputs({
+                    quantity: 10,
+                    assetType: additionalAsset.assetType,
+                    shardId: additionalAsset.shardId,
+                    recipient: await node.createP2PKHBurnAddress()
+                });
+
+                const hashWithoutScript0 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 0
+                });
+                const signature0_1 = signEcdsa(
+                    hashWithoutScript0.value,
+                    aliceSecret
+                );
+                const signature0_2 = signEcdsa(
+                    hashWithoutScript0.value,
+                    bobSecret
+                );
+
+                transfer.addInputs(additionalAsset);
+                transfer.input(1)!.setLockScript(lockScript);
+
+                const hashWithoutScript1 = transfer.hashWithoutScript({
+                    tag,
+                    type: "input",
+                    index: 1
+                });
+                const signature1_1 = signEcdsa(
+                    hashWithoutScript1.value,
+                    bobSecret
+                );
+                const signature1_2 = signEcdsa(
+                    hashWithoutScript1.value,
+                    carolSecret
+                );
+
+                transfer
+                    .input(0)!
+                    .setUnlockScript(
+                        createUnlockScript(tag, signature0_1, signature0_2)
+                    );
+
+                transfer
+                    .input(1)!
+                    .setUnlockScript(
+                        createUnlockScript(tag, signature1_1, signature1_2)
+                    );
+
+                const hash = await node.sendAssetTransaction(transfer);
+                expect(await node.sdk.rpc.chain.containsTransaction(hash)).be
+                    .true;
+                expect(await node.sdk.rpc.chain.getTransaction(hash)).not.null;
+            });
+
+            it("unlock with both outputs and inputs dynamic", async function() {
+                const tag0: SignatureTag = { input: "single", output: [0] };
+                const tag1: SignatureTag = { input: "single", output: [1] };
+
+                const hashWithoutScript0 = transfer.hashWithoutScript({
+                    tag: tag0,
+                    type: "input",
+                    index: 0
+                });
+                const signature0_1 = signEcdsa(
+                    hashWithoutScript0.value,
+                    aliceSecret
+                );
+                const signatrue0_2 = signEcdsa(
+                    hashWithoutScript0.value,
+                    bobSecret
+                );
+
+                transfer.addInputs(additionalAsset).addOutputs({
+                    quantity: 10,
+                    assetType: additionalAsset.assetType,
+                    shardId: additionalAsset.shardId,
+                    recipient: await node.createP2PKHBurnAddress()
+                });
+                transfer.input(1)!.setLockScript(lockScript);
+
+                const hashWithoutScript1 = transfer.hashWithoutScript({
+                    tag: tag1,
+                    type: "input",
+                    index: 1
+                });
+                const signature1_1 = signEcdsa(
+                    hashWithoutScript1.value,
+                    bobSecret
+                );
+                const signature1_2 = signEcdsa(
+                    hashWithoutScript1.value,
+                    carolSecret
+                );
+
+                transfer
+                    .input(0)!
+                    .setUnlockScript(
+                        createUnlockScript(tag0, signature0_1, signatrue0_2)
+                    );
+
+                transfer
+                    .input(1)!
+                    .setUnlockScript(
+                        createUnlockScript(tag1, signature1_1, signature1_2)
+                    );
+
+                const hash = await node.sendAssetTransaction(transfer);
+                expect(await node.sdk.rpc.chain.containsTransaction(hash)).be
+                    .true;
+                expect(await node.sdk.rpc.chain.getTransaction(hash)).not.null;
+            });
         });
     });
 
