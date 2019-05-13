70d50ce0b7433bf62565d4b5d3b63f226d774bff
Seulgi Kim <seulgi.kim@codechain.io>
2019-05-13 18:16:14 +0900

    Extract CommonParams

--- core/src/scheme/scheme.rs
+++ core/src/scheme/scheme.rs
@@ -19,7 +19,7 @@ use std::sync::Arc;
 
 use ccrypto::{blake256, BLAKE_NULL_RLP};
 use cjson;
-use ckey::{Address, NetworkId};
+use ckey::Address;
 use cmerkle::TrieFactory;
 use cstate::{Metadata, MetadataAddress, Shard, ShardAddress, StateDB, StateResult, StateWithCache, TopLevelState};
 use ctypes::errors::SyntaxError;
@@ -33,76 +33,11 @@ use crate::blockchain::HeaderProvider;
 
 use super::pod_state::{PodAccounts, PodShards};
 use super::seal::Generic as GenericSeal;
-use super::Genesis;
+use super::{CommonParams, Genesis};
 use crate::codechain_machine::CodeChainMachine;
 use crate::consensus::{BlakePoW, CodeChainEngine, Cuckoo, NullEngine, SimplePoA, Solo, Tendermint};
 use crate::error::{Error, SchemeError};
 use crate::header::Header;
-
-#[derive(Clone, Debug, PartialEq, Default, RlpEncodable)]
-pub struct CommonParams {
-    /// Maximum size of extra data.
-    pub max_extra_data_size: usize,
-    /// Maximum size of metadata of AssetScheme.
-    pub max_asset_scheme_metadata_size: usize,
-    /// Maximum size of metadata of TransferAsset.
-    pub max_transfer_metadata_size: usize,
-    /// Maximum size of the content of text used in store/remove actions.
-    pub max_text_content_size: usize,
-    /// Network id.
-    pub network_id: NetworkId,
-    /// Minimum transaction cost.
-    pub min_pay_transaction_cost: u64,
-    pub min_set_regular_key_transaction_cost: u64,
-    pub min_create_shard_transaction_cost: u64,
-    pub min_set_shard_owners_transaction_cost: u64,
-    pub min_set_shard_users_transaction_cost: u64,
-    pub min_wrap_ccc_transaction_cost: u64,
-    pub min_custom_transaction_cost: u64,
-    pub min_store_transaction_cost: u64,
-    pub min_remove_transaction_cost: u64,
-    pub min_asset_mint_cost: u64,
-    pub min_asset_transfer_cost: u64,
-    pub min_asset_scheme_change_cost: u64,
-    pub min_asset_supply_increase_cost: u64,
-    pub min_asset_compose_cost: u64,
-    pub min_asset_decompose_cost: u64,
-    pub min_asset_unwrap_ccc_cost: u64,
-    /// Maximum size of block body.
-    pub max_body_size: usize,
-    /// Snapshot creation period in unit of block numbers.
-    pub snapshot_period: u64,
-}
-
-impl From<cjson::scheme::Params> for CommonParams {
-    fn from(p: cjson::scheme::Params) -> Self {
-        Self {
-            max_extra_data_size: p.max_extra_data_size.into(),
-            max_asset_scheme_metadata_size: p.max_asset_scheme_metadata_size.into(),
-            max_transfer_metadata_size: p.max_transfer_metadata_size.into(),
-            max_text_content_size: p.max_text_content_size.into(),
-            network_id: p.network_id,
-            min_pay_transaction_cost: p.min_pay_cost.into(),
-            min_set_regular_key_transaction_cost: p.min_set_regular_key_cost.into(),
-            min_create_shard_transaction_cost: p.min_create_shard_cost.into(),
-            min_set_shard_owners_transaction_cost: p.min_set_shard_owners_cost.into(),
-            min_set_shard_users_transaction_cost: p.min_set_shard_users_cost.into(),
-            min_wrap_ccc_transaction_cost: p.min_wrap_ccc_cost.into(),
-            min_custom_transaction_cost: p.min_custom_cost.into(),
-            min_store_transaction_cost: p.min_store_cost.into(),
-            min_remove_transaction_cost: p.min_remove_cost.into(),
-            min_asset_mint_cost: p.min_mint_asset_cost.into(),
-            min_asset_transfer_cost: p.min_transfer_asset_cost.into(),
-            min_asset_scheme_change_cost: p.min_change_asset_scheme_cost.into(),
-            min_asset_supply_increase_cost: p.min_increase_asset_supply_cost.into(),
-            min_asset_compose_cost: p.min_compose_asset_cost.into(),
-            min_asset_decompose_cost: p.min_decompose_asset_cost.into(),
-            min_asset_unwrap_ccc_cost: p.min_unwrap_ccc_cost.into(),
-            max_body_size: p.max_body_size.into(),
-            snapshot_period: p.snapshot_period.into(),
-        }
-    }
-}
 
 /// Parameters for a block chain; includes both those intrinsic to the design of the
 /// chain and those to be interpreted by the active chain engine.
