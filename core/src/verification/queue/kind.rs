b40701de370d11a9833af05eb56d4e47802b61d0
Seulgi Kim <seulgi.kim@codechain.io>
2019-05-21 15:38:58 +0900

    Add what to validate to the name of the verification threads

--- core/src/verification/queue/kind.rs
+++ core/src/verification/queue/kind.rs
@@ -67,6 +67,8 @@ pub trait Kind: 'static + Sized + Send + Sync {
     /// The third stage: completely verified.
     type Verified: Sized + Send + BlockLike + MemUsage;
 
+    fn name() -> &'static str;
+
     /// Attempt to create the `Unverified` item from the input.
     fn create(input: Self::Input, engine: &CodeChainEngine) -> Result<Self::Unverified, Error>;
 
@@ -114,6 +116,10 @@ pub mod headers {
         type Unverified = Header;
         type Verified = Header;
 
+        fn name() -> &'static str {
+            "Headers"
+        }
+
         fn create(input: Self::Input, engine: &CodeChainEngine) -> Result<Self::Unverified, Error> {
             // FIXME: this doesn't seem to match with full block verification
             verify_header_params(&input, engine).map(|_| input)
@@ -151,6 +157,10 @@ pub mod blocks {
         type Input = Unverified;
         type Unverified = Unverified;
         type Verified = PreverifiedBlock;
+
+        fn name() -> &'static str {
+            "Blocks"
+        }
 
         fn create(input: Self::Input, engine: &CodeChainEngine) -> Result<Self::Unverified, Error> {
             match verify_block_basic(&input.header, &input.bytes, engine) {
