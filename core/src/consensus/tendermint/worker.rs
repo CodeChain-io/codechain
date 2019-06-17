f9452030b0f816275b94a565d151e2578c10f24c
Seulgi Kim <seulgi.kim@codechain.io>
2019-06-17 19:03:32 +0900

    Prefer moving to cloning

--- core/src/consensus/tendermint/worker.rs
+++ core/src/consensus/tendermint/worker.rs
@@ -1006,7 +1006,7 @@ impl Worker {
         Seal::Tendermint {
             prev_view: *last_block_view,
             cur_view: view,
-            precommits: precommits.clone(),
+            precommits,
             precommit_bitset,
         }
     }
