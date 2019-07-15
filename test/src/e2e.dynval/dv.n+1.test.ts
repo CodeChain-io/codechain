568cf36ef77894dbcb662f47326f876722f3087a
Seonpyo Kim <spkim@kodebox.io>
2019-07-15 14:49:43 +0900

    Add test case: increase nomination deposit

--- test/src/e2e.dynval/dv.n+1.test.ts
+++ test/src/e2e.dynval/dv.n+1.test.ts
@@ -146,6 +146,49 @@ describe("Dynamic Validator N -> N+1", function() {
         });
     });
 
+    describe("Increase one candidate's deposit which is less than the minimum deposit", async function() {
+        this.slow(termSeconds * 1000);
+        this.timeout(termSeconds * 2 * 1000);
+
+        const nodes = withNodes(this, {
+            promiseExpect,
+            validators: otherDynValidators
+                .map((signer, index) => ({
+                    signer,
+                    delegation: 5_000,
+                    deposit: 10_000_000 - index // tie-breaker
+                }))
+                .concat([
+                    {
+                        signer: betty,
+                        delegation: 5_000,
+                        deposit: 9999
+                    }
+                ])
+        });
+
+        it("betty should be included in validators", async function() {
+            const checkingNode = nodes[0];
+            await beforeInsertionCheck(checkingNode.sdk);
+            const nominateTx = stake
+                .createSelfNominateTransaction(checkingNode.sdk, 10_000, "")
+                .sign({
+                    secret: betty.privateKey,
+                    seq: await checkingNode.sdk.rpc.chain.getSeq(
+                        betty.platformAddress
+                    ),
+                    fee: 10
+                });
+            const nominateTxHash = checkingNode.sdk.rpc.chain.sendSignedTransaction(
+                nominateTx
+            );
+            await checkingNode.waitForTx(nominateTxHash);
+
+            await checkingNode.waitForTermChange(2, termSeconds * margin);
+            await bettyInsertionCheck(checkingNode.sdk);
+        });
+    });
+
     afterEach(async function() {
         await promiseExpect.checkFulfilled();
     });
