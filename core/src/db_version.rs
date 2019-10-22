e146ed676d748357e9e0ed51ae9994cd958b9244
Juhyung Park <jh@codechain.io>
2019-10-22 16:05:00 +0900

    Add get_version and set_version
    When the data structure of data that is saved in the KVDB is changed,
    CodeChain should migrate the data. To identify the stored data
    structure, we need to keep a version number. This commit adds code to
    save and load a version number. To make the DB easier to manage, this
    code saves versions in the same column with the same prefix.

---
+++ core/src/db_version.rs
@@ -0,0 +1,40 @@
+// Copyright 2019 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+use kvdb::{DBTransaction, KeyValueDB};
+
+use crate::db;
+
+pub const VERSION_KEY_PREFIX: &[u8] = b"version_";
+/// Save the version of Tendermint backup where the key below is pointing
+pub const VERSION_KEY_TENDERMINT_BACKUP: &[u8] = b"version_tendermint-backup";
+
+/// To support data values that are saved before the version scheme return 0 if the version does not exist
+pub fn get_version(db: &dyn KeyValueDB, key: &[u8]) -> u32 {
+    let value = db.get(db::COL_EXTRA, key).expect("Low level database error. Some issue with disk?");
+    if let Some(bytes) = value {
+        rlp::decode(&bytes)
+    } else {
+        0
+    }
+}
+
+pub fn set_version(batch: &mut DBTransaction, key: &[u8], value: u32) {
+    assert!(
+        key.starts_with(VERSION_KEY_PREFIX),
+        "Version keys should be prefixed with".to_owned() + std::str::from_utf8(VERSION_KEY_PREFIX).unwrap()
+    );
+    batch.put(db::COL_EXTRA, key, &rlp::encode(&value).into_vec());
+}
