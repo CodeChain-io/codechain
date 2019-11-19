4eff7ecb4b6d6b0b4bb17d8245e42cb3e69c5b55
Joonmo Yang <jmyang@codechain.io>
2019-11-19 20:21:22 +0900

    Use binding for match clause

--- sync/src/block/extension.rs
+++ sync/src/block/extension.rs
@@ -249,7 +249,7 @@ impl Extension {
             let body_requests: Vec<RequestMessage> = requests
                 .iter()
                 .filter_map(|r| match r {
-                    (_, RequestMessage::Bodies(..)) => Some(r.1.clone()),
+                    (_, msg @ RequestMessage::Bodies(..)) => Some(msg.clone()),
                     _ => None,
                 })
                 .collect();
@@ -257,7 +257,7 @@ impl Extension {
             let chunk_requests: Vec<RequestMessage> = requests
                 .iter()
                 .filter_map(|r| match r {
-                    (_, RequestMessage::StateChunk(..)) => Some(r.1.clone()),
+                    (_, msg @ RequestMessage::StateChunk(..)) => Some(msg.clone()),
                     _ => None,
                 })
                 .collect();
