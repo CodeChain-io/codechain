d92a604f2ba95aa768cc706a40bfd18a0dcbc654
SeongChan Lee <sclee@codechain.io>
2019-11-20 21:27:50 +0900

    Change sync extension to update pivot after transitioning to Full

--- sync/src/block/extension.rs
+++ sync/src/block/extension.rs
@@ -402,8 +402,7 @@ impl NetworkExtension<Event> for Extension {
                         if let Some(root) = restore.next_to_feed() {
                             self.send_chunk_request(&block, &root);
                         } else {
-                            cdebug!(SYNC, "Transitioning state to {:?}", State::Full);
-                            self.state = State::Full;
+                            self.transition_to_full();
                         }
                     }
                     State::Full => {
@@ -991,10 +990,17 @@ impl Extension {
             if let Some(root) = restore.next_to_feed() {
                 self.send_chunk_request(&block, &root);
             } else {
-                cdebug!(SYNC, "Transitioning state to {:?}", State::Full);
-                self.state = State::Full;
+                self.transition_to_full();
             }
         }
+    }
+
+    fn transition_to_full(&mut self) {
+        cdebug!(SYNC, "Transitioning state to {:?}", State::Full);
+        for downloader in self.header_downloaders.values_mut() {
+            downloader.update_pivot(self.client.best_block_header().hash());
+        }
+        self.state = State::Full;
     }
 }
 
