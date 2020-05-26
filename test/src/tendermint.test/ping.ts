9dd74d0968ddfc0f390cddb1bfa63147ba37d8d0
Junha Yang0 <junha@codechain.io>
2020-05-26 14:26:58 +0900

    ping test

---
+++ test/src/tendermint.test/ping.ts
@@ -0,0 +1,48 @@
+// Copyright 2018-2019 Kodebox, Inc.
+// This file is part of CodeChain.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+const { performance } = require('perf_hooks'); 
+import { expect } from "chai";
+import "mocha";
+import CodeChain from "../helper/spawn";
+
+describe("solo - 1 node", function() {
+    let node: CodeChain;
+    before(async function() {
+        node = new CodeChain();
+        await node.start();
+    });
+
+    it("ping", async function() {
+        let i = 0;
+        let t0 = performance.now()
+        for (i = 0; i < 10000; i ++) {
+            expect(await node.sdk.rpc.node.ping()).to.equal("pong");
+        }
+        let t1 = performance.now()
+        console.log(t1 - t0);
+    });
+
+    afterEach(function() {
+        if (this.currentTest!.state === "failed") {
+            node.keepLogs();
+        }
+    });
+
+    after(async function() {
+        await node.clean();
+    });    
+});
