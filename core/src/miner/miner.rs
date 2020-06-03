dbb3116e4f24940b4593294978454eff12ef6c91
Juhyung Park <jh@codechain.io>
2020-06-03 13:59:26 +0900

    Introduce NextAllowedReseal type
    NextAllowedReseal limits the lifetime of the inner lock.

--- core/src/miner/miner.rs
+++ core/src/miner/miner.rs
@@ -115,7 +115,7 @@ struct SealingWork {
 
 pub struct Miner {
     mem_pool: Arc<RwLock<MemPool>>,
-    next_allowed_reseal: Mutex<Instant>,
+    next_allowed_reseal: NextAllowedReseal,
     next_mandatory_reseal: NextMandatoryReseal,
     sealing_block_last_request: Mutex<u64>,
     sealing_work: Mutex<SealingWork>,
@@ -130,6 +130,8 @@ pub struct Miner {
     malicious_users: RwLock<HashSet<Address>>,
     immune_users: RwLock<HashSet<Address>>,
 }
+
+type NextAllowedReseal = NextMandatoryReseal;
 
 struct NextMandatoryReseal {
     instant: RwLock<Instant>,
@@ -193,7 +195,7 @@ impl Miner {
 
         Self {
             mem_pool,
-            next_allowed_reseal: Mutex::new(Instant::now()),
+            next_allowed_reseal: NextAllowedReseal::new(Instant::now()),
             next_mandatory_reseal: NextMandatoryReseal::new(Instant::now() + options.reseal_max_period),
             params: RwLock::new(AuthoringParams::default()),
             sealing_block_last_request: Mutex::new(0),
@@ -675,7 +677,7 @@ impl Miner {
 
     /// Are we allowed to do a non-mandatory reseal?
     fn transaction_reseal_allowed(&self) -> bool {
-        self.sealing_enabled.load(Ordering::Relaxed) && (Instant::now() > *self.next_allowed_reseal.lock())
+        self.sealing_enabled.load(Ordering::Relaxed) && (Instant::now() > self.next_allowed_reseal.get())
     }
 
     fn map_pending_block<F, T>(&self, f: F, latest_block_number: BlockNumber) -> Option<T>
@@ -910,7 +912,7 @@ impl MinerService for Miner {
             }
 
             // Sealing successful
-            *self.next_allowed_reseal.lock() = Instant::now() + self.options.reseal_min_period;
+            self.next_allowed_reseal.set(Instant::now() + self.options.reseal_min_period);
             if !self.options.no_reseal_timer {
                 chain.set_min_timer();
             }
