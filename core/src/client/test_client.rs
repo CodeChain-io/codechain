CodeChain-io/foundry

Remove unnecessary serialize/deserialize while calling import_header

core/src/client/test_client.rs
@@ -458,7 +458,7 @@ impl ImportBlock for TestBlockChainClient {
         Ok(h)
     }
 
-    fn import_header(&self, _bytes: Bytes) -> Result<BlockHash, BlockImportError> {
+    fn import_header(&self, _bytes: Header) -> Result<BlockHash, BlockImportError> {
         unimplemented!()
     }
 

CodeChain-io/codechain

core/src/client/test_client.rs
@@ -508,7 +508,7 @@ impl ImportBlock for TestBlockChainClient {
         Ok(h)
     }
 
-    fn import_header(&self, _bytes: Bytes) -> Result<BlockHash, BlockImportError> {
+    fn import_header(&self, _bytes: BlockHeader) -> Result<BlockHash, BlockImportError> {
         unimplemented!()
     }
 