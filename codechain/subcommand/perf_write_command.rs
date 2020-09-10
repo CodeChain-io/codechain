a4528019e93da173cd63f7add53a0497f562c1f7
Juhyung Park <jh@codechain.io>
2020-09-10 19:16:50 +0900

    Make the number of write customizable

--- codechain/subcommand/perf_write_command.rs
+++ codechain/subcommand/perf_write_command.rs
@@ -1,21 +1,31 @@
 use ccore::ClientConfig;
+use ckey::Address;
 use clap::ArgMatches;
 use clogger::{self, LoggerConfig};
+use cstate::{StateDB, StateWithCache, TopLevelState, TopState};
 use kvdb::KeyValueDB;
 use kvdb_rocksdb::{Database, DatabaseConfig};
-use std::{path::Path, sync::Arc, time::Instant};
-use ckey::Address;
-use cstate::{TopState, TopLevelState, StateDB, StateWithCache};
 use primitives::H256;
+use std::{path::Path, sync::Arc, time::Instant};
 
 pub const COL_STATE: Option<u32> = Some(0);
 pub const COL_EXTRA: Option<u32> = Some(3);
 
-pub fn run_perf_write_command(_matches: &ArgMatches) -> Result<(), String> {
+pub fn run_perf_write_command(matches: &ArgMatches) -> Result<(), String> {
     clogger::init(&LoggerConfig::new(777), None).expect("Logger must be successfully initialized");
+
+    let num = matches.value_of("number").unwrap();
+    let num: u32 = num.parse().unwrap();
+
+    println!("start");
     let db = open_db()?;
+    println!("open db");
     let journal_db = journaldb::new(Arc::clone(&db), journaldb::Algorithm::Archive, COL_STATE);
+    println!("open journal db");
     let state_db = StateDB::new(journal_db);
+    println!("open state db");
+
+    println!("before getting root");
 
     let root = {
         let bytes = db.get(COL_EXTRA, b"perf_data_root").unwrap().unwrap();
@@ -24,7 +34,9 @@ pub fn run_perf_write_command(_matches: &ArgMatches) -> Result<(), String> {
 
     let mut toplevel_state = TopLevelState::from_existing(state_db.clone(&root), root).unwrap();
 
-    for i in 0..10 {
+    println!("before loop");
+    for i in 0..10_u64.pow(num) {
+        // println!("loop");
         let now = Instant::now();
         let address = Address::random();
         toplevel_state.add_balance(&address, i).unwrap();
